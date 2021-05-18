f070784a9ad774b93e4cf079bef8050e557391c9
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-05-18 20:48:44 -0300

    correçoes no lint, js

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ confirma.addEventListener('change', () => {
 const senha = document.querySelector('#senha').value;
 const email = document.querySelector('#email').value;
 const botaoHeader = document.querySelector('#btnheader');
-botaoHeader.addEventListener('click', function () {
+botaoHeader.addEventListener('click', function() {
   const novaSenha = document.querySelector('#senha').value;
   const novoEmail = document.querySelector('#email').value;
   if (senha !== novaSenha || email !== novoEmail) {
-      alert('Login ou senha inválidos.');
+    alert('Login ou senha inválidos.');
   } else if (senha === novaSenha && email === novoEmail) {
-      alert('Olá, Tryber!');
+    alert('Olá, Tryber!');
   }
 });
