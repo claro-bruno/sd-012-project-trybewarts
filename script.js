2497ddbf49872bfc8eec192267a6a6ca2cf03e0f
matheustkaczyk <matheus111904@hotmail.com>
2021-05-18 17:14:55 -0400

    correcoes lint

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const emailLogin = document.getElementById('email-login');
 const senhaLogin = document.getElementById('senha-login');
-const submitLogin = document.getElementById('submit-login')
-const formLogin = document.getElementsByClassName('trybewarts-login')
+const submitLogin = document.getElementById('submit-login');
+const formLogin = document.getElementsByClassName('trybewarts-login');
 
-formLogin.addEventListener('submit', (event) =>{
-    event.preventDefault();
+formLogin.addEventListener('submit', (event) => {
+  event.preventDefault();
 });
 
 function validaLogin (){
-    if (emailLogin.value === 'tryber@teste.com'){
-        alert('Olá, Tryber!')
-    }else if (senhaLogin.value === '123456') {
-        alert('Olá, Tryber!')
-    }else {
-        alert('Login ou senha inválidos.')
-    }
-};
+  if (emailLogin.value === 'tryber@teste.com'){
+    alert('Olá, Tryber!');
+  }else if (senhaLogin.value === '123456') {
+    alert('Olá, Tryber!');
+  }else {
+    alert('Login ou senha inválidos.');
+  }
+}
 
 submitLogin.addEventListener('click', validaLogin);
