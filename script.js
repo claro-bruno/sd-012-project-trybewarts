41d44eda9472013f492250d4ba0299e6c8cbab87
Renata Nunes <renatanunes231@yahoo.com.br>
2021-05-18 16:52:47 -0300

    Ajustando formatacao da funcao

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const botaoLogar = document.getElementById('logar');
 
-botaoLogar.addEventListener('click', alerta());
+botaoLogar.addEventListener('click', verificaLogin);
 
-function alerta() {
+function verificaLogin() {
   if (email.value !== 'tryber@teste.com' || senha.value !== '123456') {
-    alert('Login ou senha inválidos.');
+    return alert('Login ou senha inválidos.');
   } else {
-    alert('Olá, Tryber!');
+    return alert('Olá, Tryber!');
   }
 }
