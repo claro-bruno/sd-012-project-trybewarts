6de963f7be435ba6a8a6c71b5d281ecdadc82006
Çuiz Henrique <luishenriqueee2010@hotmail.com>
2021-05-18 17:35:28 -0300

    Adiciona desafio 3

---
+++ script.js
@@ -0,0 +1,11 @@
+function validate() {
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+  if (email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+const submitButton = document.querySelector('#send-button');
+submitButton.addEventListener('click', validate);
