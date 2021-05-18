ab845b1162a6b16df805ea656ddddd9a21919e53
Samuel Constantino <samuelicapuidados@gmail.com>
2021-05-18 17:09:05 -0300

    atualiza padrões do lint

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // acessa elemento botão de login
 // const botaoLogin = document.querySelector('#botaoLogin');
 
-let listener = function (event) {
-    if (event.target.id === 'botaoLogin') {
-        const inputLogin = document.querySelector('#inputLogin');
-        const inputSenha = document.querySelector('#inputSenha');
+const listener = function (event) {
+  if (event.target.id === 'botaoLogin') {
+    const inputLogin = document.querySelector('#inputLogin');
+    const inputSenha = document.querySelector('#inputSenha');
 
-        if (inputLogin.value === 'tryber@teste.com' && inputSenha.value === '123456') {
-            alert('Olá, Tryber!');
-        } else {
-            alert('Login ou senha inválidos.');
-            event.preventDefault();
-        }
+    if (inputLogin.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Login ou senha inválidos.');
+      event.preventDefault();
     }
+  }
 };
 
 document.addEventListener('click', listener);
