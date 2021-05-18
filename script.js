c0da5b2123dc38b948bc5be28fe710685e262105
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-05-18 17:56:13 -0300

    Requisito 2 e 3 (falta finalizar CSS)

---
+++ script.js
@@ -0,0 +1,12 @@
+let login = document.getElementById("input-login");
+let senha = document.getElementById("input-senha");
+let botao = document.getElementById("input-botao");
+
+function mensagemFormulario(){
+    if(login.value === "tryber@teste.com" && senha.value === "123456") {
+        alert("Olá, Tryber!")
+    } else{
+        alert("Login ou senha inválidos")
+    };
+};
+botao.addEventListener("click", mensagemFormulario);
