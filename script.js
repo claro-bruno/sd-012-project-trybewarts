29195e4356f0f022f8e26e3f9a806ba1f99aca83
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-18 18:03:52 -0300

    Adiciona requisito 14

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const emailLogin = document.getElementById('email-login');
 const passwordLogin = document.getElementById('password-login');
+const rate = document.getElementById('rate-container');
 
 function login() {
   if (emailLogin.value === 'tryber@teste.com' && passwordLogin.value === '123456') {
@@ -11,3 +12,18 @@ function login() {
 
 const formLogin = document.getElementsByClassName('trybewarts-login');
 formLogin[0].addEventListener('submit', login);
+
+function rates() {
+  for(let index = 1; index <= 10; index += 1) {
+    let rateOption = document.createElement('input');
+    let rateLabel = document.createElement('label');
+    rateOption.value = index;
+    rateOption.name = "rate";
+    rateOption.type = "radio";
+    rateLabel.innerHTML = index;
+    rate.appendChild(rateOption);
+    rate.appendChild(rateLabel);
+  }
+}
+
+rates();
