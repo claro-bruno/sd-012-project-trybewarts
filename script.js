d136cc1f681c21d5c4ca43890ef09116c1d21aff
Julio Pereira <jandrade.pereira1@gmail.com>
2021-05-18 17:12:47 -0300

    Incluir os pontos importantes do requisito 3

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const btnLogin = document.getElementById('btn-login');
 
+function signIn() {
+  const login = document.getElementById('login').value;
+  const password = document.getElementById('password').value;
+  if (login === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+
+btnLogin.addEventListener('click', signIn);
