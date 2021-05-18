e2c76cc6e85581d088f8bae0684059ea6394c9d7
Alexander <alexanderteodorocastro95@hotmail.com>
2021-05-18 18:40:33 -0300

    requisito 4

---
+++ script.js
@@ -0,0 +1,13 @@
+let pegabotao = document.querySelector('#botao');
+function clicarLogar(){
+    let email1 = document.getElementById('email').value.toLowerCase();
+    let password2 = document.getElementById('password').value;    
+    if (email1 === 'tryber@teste.com' && password2 === '123456'){
+        alert('Olá, Tryber!')
+    }else{
+        alert('Login ou senha inválidos.')
+        email1 = '';
+        password2 = '';
+    }
+}
+pegabotao.addEventListener('click', clicarLogar )
