9b48ef6cde41768e050429e8b808df66fa790c3a
Renan_Miyashita <renanmiyashita@gmail.com>
2021-05-18 16:48:27 -0300

    requisitos 1,2,3 com lint

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const inputButton = document.getElementById('inputButton');
 
-function validateLogin () {
-    const inputLogin = document.getElementById('inputLogin').value;
-    const inputPassword = document.getElementById('inputPassword').value;
-    if (inputLogin === 'tryber@teste.com' && inputPassword === '123456') {
-        alert ('Olá, Tryber')
-    } else {
-        alert ('Login ou Senha inválidos')
-    }
-};
+function validateLogin() {
+  const inputLogin = document.getElementById('inputLogin').value;
+  const inputPassword = document.getElementById('inputPassword').value;
+  if (inputLogin === 'tryber@teste.com' && inputPassword === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
 
 inputButton.addEventListener('click', validateLogin);
 
