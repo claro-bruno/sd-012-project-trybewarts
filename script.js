0b0a0430caa338ea0bac7d7253aaad73d1b43e16
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-05-18 17:26:10 -0300

    Corrige Lint requisito 3

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const botaoInput = document.getElementById('botao');
-//validacao de login
-botaoInput.addEventListener('click', function() {
-    const inputLogin = document.getElementById('email').value;
-    const inputPassword = document.getElementById('senha').value;
-    if (inputLogin === 'tryber@teste.com' && inputPassword === '123456') {
-        alert ('Olá, Tryber!')
-    } else {
-        alert ('Login ou senha inválidos.')
-    }
-})
+//  validacao de login
+function loginValidate() {
+  const inputLogin = document.getElementById('email').value;
+  const inputPassword = document.getElementById('senha').value;
+  if (inputLogin === 'tryber@teste.com' && inputPassword === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+
+botaoInput.addEventListener('click', loginValidate);
