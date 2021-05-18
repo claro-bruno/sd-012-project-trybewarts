eb434286ff05c81794b4b33fce51c8b9b4ea6220
Heder Waine <hederw@hotmail.com>
2021-05-18 17:37:58 -0300

    Requisito 15

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const login = document.getElementById('login');
-const password = document.getElementById('password')
+const password = document.getElementById('password');
 
 function loginCheck() {
-  if (login.value === 'tryber@teste.com' && password.value === '123456'){
+  if (login.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
     return;
   }
   alert('Login ou senha inválidos.');
-  return;
 }
-function habilitarBtn(checkBox){
-  
-  if(checkBox.checked){      
-      document.getElementById("submit-btn").disabled = false;
-  } else{      
-      document.getElementById("submit-btn").disabled = true;
+loginCheck();
+
+//  referência : https://thisinterestsme.com/disable-button-checkbox-checked/
+
+function habilitarBtn(checkBox) {
+  if (checkBox.checked) {
+    document.getElementById('submit-btn').disabled = false;
+  } else {
+    document.getElementById('submit-btn').disabled = true;
   }
 }
+habilitarBtn();
