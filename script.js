d4966a36cc256504ed2178e320fb5513befa0a92
ViniciusMossi <vini_agg@hotmail.com>
2021-05-18 19:30:21 -0400

    Verifica identacaçao

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let botao = document.getElementById('botao');
-let email = document.getElementById('email');
-let senha = document.getElementById('senha');
-console.log(email.value);
+const botao = document.getElementById('botao');
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
 
-function validar () {
-if ((email.value !== 'tryber@teste.com') || (senha.value !== '123456')) {
-  alert('Login ou senha inválidos.')
-} else {
-  alert('Olá, Tryber!')};
+function validar() {
+  if ((email.value !== 'tryber@teste.com') || (senha.value !== '123456')) {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
 }
 
 function preventDef(event) {
   event.preventDefault();
 }
 
-botao.addEventListener('click', preventDef); 
-botao.addEventListener('click', validar); 
+botao.addEventListener('click', preventDef);
+botao.addEventListener('click', validar);
 
-function radio () {
-  let divRadio = document.getElementById('radio-conteiner');
+function radio() {
+  const divRadio = document.getElementById('radio-conteiner');
   for (let index = 1; index < 11; index += 1) {
-    let criaInput = document.createElement('input');
-    let criaLabel = document.createElement('label');
+    const criaInput = document.createElement('input');
+    const criaLabel = document.createElement('label');
     criaInput.type = 'radio';
     criaInput.name = 'rate';
     criaLabel.innerText = index;
@@ -34,10 +34,9 @@ function radio () {
 }
 radio();
 
-// Encontramos resolução através do link : https://stackoverflow.com/questions/18110865/how-to-disable-enable-a-button-with-a-checkbox-if-checked 
-let checker = document.getElementById('agreement');
-let sendbtn = document.getElementById('submit-btn');
-checker.onchange = function funcao () {
-sendbtn.disabled = !this.checked;
+// Encontramos resolução através do link : https://stackoverflow.com/questions/18110865/how-to-disable-enable-a-button-with-a-checkbox-if-checked
+const checker = document.getElementById('agreement');
+const sendbtn = document.getElementById('submit-btn');
+checker.onchange = function funcao() {
+  sendbtn.disabled = !this.checked;
 };
-
