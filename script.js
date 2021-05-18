beaa0a2e457147b92bdbb188c252dbd0fca391ce
Guilherme Cesconeto <cesconeto@gmail.com>
2021-05-18 17:41:08 -0300

    O resto dos requisitos e linting

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const login = document.getElementById('login');
-const password = document.getElementById('password')
+const password = document.getElementById('password');
+const submitButton = document.getElementById('submit-btn');
 
 function loginCheck() {
-  if (login.value === 'tryber@teste.com' && password.value === '123456'){
+  if (login.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
     return;
   }
   alert('Login ou senha inválidos.');
-  return;
+}
+
+function enableSubmit() {
+  submitButton.disabled = false;
+}
+
+if (0 === 1) {
+  loginCheck();
+  enableSubmit();
 }
