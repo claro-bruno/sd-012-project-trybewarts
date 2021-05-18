2b9429b7b894eff88273b490061229ad897cc2cc
Wellington <junior.wel02@exemplo.br>
2021-05-18 16:15:57 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,11 @@
+const botaoLogar = document.querySelector("#botaoLogar");
+const login = document.querySelector("#login");
+const senha = document.querySelector("#senha");
+
+botaoLogar.addEventListener("click", function () {
+  if ((login.value === "tryber@teste.com") & (senha.value === "123456")) {
+    window.alert("Olá, Tryber!");
+  } else {
+    window.alert("Login ou senha inválidos.");
+  }
+});
