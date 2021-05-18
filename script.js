517eca71ec40e3cb33f30f472161d85dd6469c03
Teika <shimworks.tk@gmail.com>
2021-05-18 16:18:27 -0300

    resolve requisito 09

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function validateLogin () {
 };
 
 inputButton.addEventListener('click', validateLogin);
+
+// Função que adiciona Option ao select de ID house
+const select = document.getElementById('house');
+const optionID = ['gitnoria-house', 'reactpuff-house', 'corvinode-house', 'pytherina-house'];
+const optionValue = ['Gitnória', 'Reactpuff', 'Corvinode', 'Pytherina'];
+function createOptions() {
+  for (let index = 0; index < 4; index += 1) {
+    const option = document.createElement('option');
+    option.value = optionValue[index];
+    option.id = optionID[index];
+    option.innerHTML = optionValue[index];
+    select.appendChild(option);
+  }
+}
+createOptions();
