6a7e50b40519668d44911fb900b86eb69985abae
Adryan Rosa <adryan.lmrosa@gmail.com>
2021-05-18 16:53:42 -0300

    Adiciona rodapé

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const emailLogin = document.getElementById('email-login');
 const passwordLogin = document.getElementById('password-login');
 
 function login() {
-  if (
-    emailLogin.value === 'tryber@teste.com'
-    && passwordLogin.value === '123456'
-  ) {
+  if (emailLogin.value === 'tryber@teste.com' && passwordLogin.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
