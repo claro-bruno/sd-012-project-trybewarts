9a80f5042b1edea439b815ed0d5e2838f40a4b94
gscuri <gscuri6@hotmail.com>
2021-05-18 19:24:16 -0300

    exercicio 4

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const selectLogin = document.querySelector('#login');
 const selectPassword = document.querySelector('#senha');
 
 function verificarLogin() {
-  if (selectLogin.value === 'tryber@teste.com' && selectPassword.value === '123456') {
+  if (
+    selectLogin.value === 'tryber@teste.com'
+  ) {
+    alert('Olá, Tryber!');
+  } else if (selectPassword.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
