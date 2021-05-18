a2622a7b97f33c864894c3cf1fd1581a2129d848
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-05-18 16:08:28 -0300

    requisito 3 tentando problema

---
+++ script.js
@@ -0,0 +1,15 @@
+const login = document.querySelector('#login');
+const senha = document.querySelector('#senha');
+const button = document.querySelector('#buttonEntrar');
+const form = document.querySelector('.trybewarts-login');
+
+function verificaLogin () {
+    if(login.value !== 'tryber@teste.com' && senha.value !== '123456') {
+        alert('Login ou senha inválidos.');
+    }
+    else {
+        alert('Olá, Tryber!');
+    }
+}
+
+form.addEventListener('submit', verificaLogin());
