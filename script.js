fa52f0a7178eca24a7fd30459b017aeaff6943a2
Eric Kreis <erickreis793@gmail.com>
2021-05-18 19:44:05 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const loginButton = document.getElementById('button-login');
 const inputLogin = document.getElementById('input-login');
 const agreement = document.getElementById('agreement');
 const submitButton = document.getElementById('submit-btn');
+const textarea = document.getElementById('textarea');
+const counter = document.getElementById('counter');
 
 function loginButtonExec() {
   loginButton.addEventListener('click', (event) => {
@@ -24,5 +26,14 @@ function submitButtonExec() {
   });
 }
 
+function textareaExec() {
+  let countNum = 0;
+  textarea.addEventListener('keyup', () => {
+    countNum = 500 - textarea.value.length;
+    counter.innerHTML = countNum;
+  });
+}
+
 loginButtonExec();
+textareaExec();
 submitButtonExec();
