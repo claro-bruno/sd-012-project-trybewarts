942dfb54870356c3f7e833bbb7e070baa037bb9a
Augusto <augustocm2002@yahoo.com.br>
2021-05-18 18:34:01 -0300

    Requisito 3 e 4 - Corrigindo eslint

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function alertClick() {
   if (login.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
-    alert('Login ou senha inválidos.');   
-  } 
- }
+    alert('Login ou senha inválidos.');
+  }
+}
 clickButton.addEventListener('click', alertClick);
