cff065d8086bc00066ed36a2ab361118599ba015
Sarah <sarahgsb@hotmail.com>
2021-05-18 17:04:44 -0300

    requisito 3

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const header = getElementsByClass('header');
+const login = document.getElementById('login');
+const password = document.getElementById('password');
+const loginButton = document.getElementById('login-btn');
+const loginInput = 'tryber@teste.com';
+const passwordInput = '123456';
+
+function loginValidate() {
+  if (login.value === loginInput && password.value === passwordInput) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+
+loginButton.addEventListener('click', loginValidate);
