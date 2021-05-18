fa50624819c5468200abd50c4849a279c995ca4c
Wellington <junior.wel02@exemplo.br>
2021-05-18 17:13:39 -0300

    correçao eslint

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const botaoLogar = document.querySelector('#botaoLogar');
 const login = document.querySelector('#login');
 const senha = document.querySelector('#senha');
 
-botaoLogar.addEventListener('click', function verificaLogin() {
+botaoLogar.addEventListener('click', () => {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
-    window.alert('Olá, Tryber!');
-  } else {
-    window.alert('Login ou senha inválidos.');
+    return window.alert('Olá, Tryber!');
+  }
+  if (login.value !== 'tryber@teste.com' && senha.value !== '123456') {
+    return window.alert('Login ou senha inválidos.');
   }
 });
