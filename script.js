ea9a73d83dcd97693f9ed687316cff8c15b6e556
Eric Kreis <erickreis793@gmail.com>
2021-05-18 18:57:44 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const loginButton = document.getElementById('button-login');
 const inputLogin = document.getElementById('input-login');
+const agreement = document.getElementById('agreement');
+const submitButton = document.getElementById('submit-btn');
 
 function loginButtonExec() {
   loginButton.addEventListener('click', (event) => {
@@ -12,4 +14,15 @@ function loginButtonExec() {
   });
 }
 
+function submitButtonExec() {
+  agreement.addEventListener('click', () => {
+    if (agreement.checked) {
+      submitButton.disabled = false;
+    } else {
+      submitButton.disabled = true;
+    }
+  });
+}
+
 loginButtonExec();
+submitButtonExec();
