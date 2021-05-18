536a36bd1496e6e2903f4654bf6fbf600e61781d
lucasnahas <lucasahas@gmail.com>
2021-05-18 19:16:25 -0300

    adiciona a logica de validacao de login

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-// login errado: alert "Login ou senha inválidos"
-// login certo; alert "Olá, Tryber!"
+
+// Requisito 3
+let loginSubmitBtn = document.getElementById('login-submit');
+let loginUser = document.getElementById('login-name');
+let loginPwd = document.getElementById('login-pwd');
+loginSubmitBtn.addEventListener('click', validateLogin);
+function validateLogin(e){
+  e.preventDefault();
+  let user = loginUser.value;
+  let pwd = loginPwd.value;
+  if (user == 'tryber@teste.com' && pwd == '123456'){
+    alert('Olá, Tryber!');
+  }else{
+    alert('Login ou senha inválidos.');
+  }
+}
+
