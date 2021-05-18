118fcd63dac17b60e3e784c52bb88909dfa6cb75
gscuri <gscuri6@hotmail.com>
2021-05-18 18:10:51 -0300

    exercicio 3 com lint corrigidousando prettier

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const selectButton = document.querySelector('.botaoSubmit');
 const selectLogin = document.querySelector('#login');
 const selectPassword = document.querySelector('#senha');
 
-function verificarLogin(event) {
-    if (
-        selectLogin.value === 'tryber@teste.com' &&
-        selectPassword.value === '123456'
-        ) {
-            alert('Olá, Tryber!');
-        } else {
-            alert('Login ou senha inválidos.');
-        }
-    };
+function verificarLogin() {
+  if (
+    selectLogin.value === 'tryber@teste.com'
+  ) {
+    alert('Olá, Tryber!');
+  } else if (selectPassword.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
 selectButton.addEventListener('click', verificarLogin);
-    
