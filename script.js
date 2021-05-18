44bfa4dc98c1aea68678ab8c8d9b42add71a04d4
Henrique Remedios <henriqueremedios@gmail.com>
2021-05-18 19:20:33 -0300

    Refatorando requisito 8

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function Startlogin() {
         alert("Login ou Senha inválidos.");
     }
 };
+
+function addHouse() {
+    
+    const selectHouse = document.getElementById('house');
+    const optionsHouse = ['gitnoria-house', 'reactpuff-house', 'corvinode-house', 'pytherina-house'];
+
+    for (index= 0; index < optionsHouse.length; index += 1) {
+        houses = optionsHouse[index]
+    
+     const option = document.createElement('option');
+        option.value = houses;
+        option.text = houses;
+        selectHouse.appendChild(option);
+    };
+};
