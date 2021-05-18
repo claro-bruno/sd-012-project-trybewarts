7bb49882167e001745ebdf9c03c99502bfaec1bd
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-05-18 16:18:02 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,16 @@
+const { RuleTester } = require("eslint");
+
+window.onload = function() {
+    let btn = document.querySelector('.btn-submit');
+    btn.addEventListener('click', function() {
+        let login = document.querySelector('.input-login');
+        let loginValue = login.value;
+        let password = document.querySelector('.input-senha');
+        let passwordValue = password.value;
+        if(loginValue == 'tryber@RuleTester.com' && passwordValue == 123456){
+            alert('Olá, Tryber!');
+        } else {
+            alert('Login ou senha inválidos.');
+        }
+    })
+}
