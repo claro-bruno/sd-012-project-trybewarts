2f0b4fb1d4fda34abcb484fb0822c0807d1a743e
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-05-18 20:14:03 -0300

    corrige lints do script

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+const email = document.querySelector('#input-login-email');
+const password = document.querySelector('#input-login-password');
+const loginButton = document.querySelector('#loginButton');
 function checkLogin() {
-    const insertedEmail = email.value;
-    const insertedPassword = password.value;
-    const emailFormat = 'tryber@teste.com';
-    const passwordFormat = '123456';
-    if ((insertedEmail !== emailFormat) || (insertedPassword !== passwordFormat)) {
-      email.value = '';
-      password.value = '';
-      alert('Login ou senha inválidos.');
-    } else {
-      alert('Olá, Tryber!');
-    }
+  const insertedEmail = email.value;
+  const insertedPassword = password.value;
+  const emailFormat = 'tryber@teste.com';
+  const passwordFormat = '123456';
+  if ((insertedEmail !== emailFormat) || (insertedPassword !== passwordFormat)) {
+    email.value = '';
+    password.value = '';
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
   }
-  
-  loginButton.addEventListener('click', checkLogin);
-  
+}
+loginButton.addEventListener('click', checkLogin);
