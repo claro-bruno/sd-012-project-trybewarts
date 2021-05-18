c6859ec0a6160e257d14504e42e483fdff39ec67
Eduardo Linhares <modsgtasagame@gmail.com>
2021-05-18 17:46:45 -0300

    Resolve ESlint

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-let loginButton = document.querySelector("#login-button");
-let loginInput = document.querySelector("#login-input");
-let passInput = document.querySelector("#pass-input");
-let escolheCasa = document.querySelector("#house");
+const loginButton = document.querySelector('#login-button');
+const loginInput = document.querySelector('#login-input');
+const passInput = document.querySelector('#pass-input');
+const escolheCasa = document.querySelector('#house');
 
-escolherCasa();
-
-loginButton.addEventListener("click", (event) => {
+loginButton.addEventListener('click', (event) => {
   event.preventDefault();
-  if (loginInput.value != "tryber@teste.com" || passInput.value != "123456") {
-    alert("Login ou senha inválidos.");
+  if (loginInput.value !== 'tryber@teste.com' || passInput.value !== '123456') {
+    alert('Login ou senha inválidos.');
   } else {
-    alert("Olá, Tryber!");
-    loginInput.value = "";
-    passInput.value = "";
+    alert('Olá, Tryber!');
+    loginInput.value = '';
+    passInput.value = '';
   }
 });
 
 function escolherCasa() {
-  let casas = [
-    { casa: 'Gitnória', id: 'gitnoria-house'},
+  const casas = [
+    { casa: 'Gitnória', id: 'gitnoria-house' },
     { casa: 'Reactpuff', id: 'reactpuff-house' },
     { casa: 'Corvinode', id: 'corvinode-house' },
     { casa: 'Pytherina', id: 'pytherina-house' },
   ];
 
   for (let index = 0; index < casas.length; index += 1) {
-    let novaCasa = document.createElement("option");
+    const novaCasa = document.createElement('option');
     novaCasa.innerHTML = casas[index].casa;
     novaCasa.id = casas[index].id;
     escolheCasa.appendChild(novaCasa);
   }
 }
+
+escolherCasa();
