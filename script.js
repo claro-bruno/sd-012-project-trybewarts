2f0cbd16799d648bdc110ca6ac5709ce3246107a
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-05-18 19:48:17 -0300

    arrumado lint do script

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const loginButton = document.getElementById('botao-login');
 function checkLogin() {
   let loginField = document.getElementById("login-field");
   let senha = document.getElementById("senha");
-  (loginField.value.toString() === 'tryber@teste.com') && (senha.value.toString() === '123456') ?
-    alert('Olá, Tryber!') :
-    alert('Login ou senha inválidos.');
+  (loginField.value.toString() === 'tryber@teste.com') && (senha.value.toString() === '123456')
+    ? alert('Olá, Tryber!')
+    : alert('Login ou senha inválidos.');
 }
 loginButton.addEventListener('click', checkLogin);
