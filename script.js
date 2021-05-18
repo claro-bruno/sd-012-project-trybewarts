41ef5b6426b69edd6c75d510d8bcf6252c71ecb3
Anderson Barbosa <andbmarques@gmail.com>
2021-05-18 17:42:26 -0300

    Requisitos 16, 17 e 18 adicionados

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ function formLogin() {
   });
 }
 
+function submitForm() {
+  const agreementCheckbox = document.getElementById('agreement');
+  const submitBtn = document.getElementById('submit-btn');
+  agreementCheckbox.addEventListener('click', () => {
+    submitBtn.removeAttribute('disabled');
+  });
+}
+
 window.onload = function loadPage() {
   formLogin();
+  submitForm();
 };
