ebe765fa95b9688531b3200e12f77c059f8fb8b8
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-05-18 17:28:21 -0300

    Resolvendo requisito 8

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const senha = document.querySelector('#senha');
 const btn = document.querySelector('#buttonEntrar');
 
 function verificaLogin() {
-  if (login.value !== 'tryber@teste.com' && senha.value !== '123456') {
-    alert('Login ou senha inválidos.');
-  } else {
+  if (login.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
   }
 }
 
