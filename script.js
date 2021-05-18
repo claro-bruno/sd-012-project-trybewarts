a08886a8ab7fdaf054702da6d76449515ded6ab6
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-18 18:07:30 -0300

    adiciona requisito 9

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let getEmail = document.querySelector("#email");
-let getPassword = document.querySelector("#password");
-let getButtunLogin = document.querySelector("#btn-login");
+const getEmail = document.querySelector("#email");
+const getPassword = document.querySelector("#password");
+const getButtunLogin = document.querySelector("#btn-login");
+const getHouse = document.querySelector("#house");
+let tiposCasa = [{valor: 'Gitnória', id: 'gitnoria-house'}, {valor: 'Reactpuff', id: 'reactpuff-house'}, {valor: 'Corvinode', id: 'corvinode-house'}, {valor: 'Pytherina', id: 'pytherina-house'}];
 
 function verificarLogin(email, password) {
   let emailCerto = "tryber@teste.com";
@@ -13,7 +15,20 @@ function verificarLogin(email, password) {
   }
   console.log(getEmail.value, getPassword.value);
 }
+
+function criaOpcaoCasa() {
+  for (const tipo of tiposCasa) {
+    const opcao = document.createElement('option');
+    opcao.value = tipo.valor;
+    opcao.innerText = tipo.valor;
+    opcao.id = tipo.id;
+    getHouse.appendChild(opcao);
+  }
+}
+
 getButtunLogin.addEventListener(
   "click",
   verificarLogin(getEmail.value, getPassword.value)
 );
+
+criaOpcaoCasa();
