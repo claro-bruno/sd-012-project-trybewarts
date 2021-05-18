752435ecf0e72317e0ed5d75bc122eb8e0cdc72a
Thiago Prado <thiagopc_dev@hotmail.com>
2021-05-18 17:03:58 -0300

    Desafio 3 e 4

---
+++ script.js
@@ -0,0 +1,28 @@
+function getLogin() {
+    const login = document.getElementById('input-login').value;
+    return login;
+}
+
+function getSenha() {
+    const senha = document.getElementById('input-senha').value;
+    return senha;
+}
+
+function validateLogin(login, senha) {
+    if ((login === 'tryber@teste.com') && (senha === '123456')) return true;
+}
+
+function loginButton() {
+    const button = document.getElementById('btt-login');
+    button.addEventListener('click', () => {
+        if (validateLogin(getLogin(), getSenha())) {
+            alert('Olá, Tryber!');
+        } else {
+            alert('Login ou senha inválidos.');
+        }
+    });
+}
+
+window.onload = () => {
+    loginButton();
+}
