2e37b8932eebf2f1ef669ba944ff89f406208a90
igor <igorsife80@gmail.com>
2021-05-18 17:23:23 -0300

    resolve os requisitos 2 e 3

---
+++ script.js
@@ -0,0 +1,11 @@
+/*let btnLogin = document.getElementsByClassName('login-button');
+btnLogin.addEventListner('click', alerta)
+
+function alerta(event) {
+  //event.target.
+  if (condition) {
+    window.alert('Login ou senha inválidos.')
+  } else {
+    window.alert('Olá, Tryber!')
+  }
+}*/
