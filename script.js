330c20eae4fe0e3515630f2ea63b3bec9652cfb9
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-05-18 16:40:05 -0300

    ajustando code

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const login = document.querySelector('#login');
 const senha = document.querySelector('#senha');
 const btn = document.querySelector('#buttonEntrar');
-const form = document.querySelector('.trybewarts-login');
 
-function verificaLogin () {
-  if(login.value !== 'tryber@teste.com' && senha.value !== '123456') {
+function verificaLogin() {
+  if (login.value !== 'tryber@teste.com' && senha.value !== '123456') {
     alert('Login ou senha inválidos.');
-  }
-  else {
+  } else {
     alert('Olá, Tryber!');
   }
 }
