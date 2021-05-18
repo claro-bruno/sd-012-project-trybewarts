0ce00eb3f85b083f675567e5c00187eacf746f48
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-05-18 17:17:33 -0300

    Resolve os requisitos 3 e 4

---
+++ script.js
@@ -0,0 +1,14 @@
+let password = document.getElementById('password');
+let correctPassword = (123456);
+let login = document.getElementById('login');
+let correctLogin = "tryber@teste.com"
+let logar = document.getElementById('login-button')
+
+
+logar.addEventListener('click', function (){
+    if (password.value == correctPassword && login.value === correctLogin) {
+        alert('Olá, Tryber!');    
+    } else {
+        alert("Login ou senha inválidos");
+    }
+})
