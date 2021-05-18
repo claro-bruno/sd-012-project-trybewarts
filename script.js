05ab62ad25afc58fb1f607ea37876035bd317650
matheustkaczyk <matheus111904@hotmail.com>
2021-05-18 18:25:48 -0400

    requesito 3 finalizado

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const emailLogin = document.getElementById('email-login');
 const senhaLogin = document.getElementById('senha-login');
-const submitLogin = document.getElementsByClassName('submit-login')[0];
+const submitLogin = document.querySelector('.submit-login');
 const formLogin = document.getElementById('form-login');
 const emailPadrao = "tryber@teste.com";
 const senhaPadrao = "123456"
 
-submitLogin.addEventListener('click', (event) => {
+formLogin.addEventListener('submit', (event) => {
   event.preventDefault();
   console.log('formulário enviado')
 });
 
-
 function validaLogin() {
-  if (emailLogin.value === emailPadrao || senhaLogin.value === senhaPadrao) {
+  if (emailLogin.value === emailPadrao && senhaLogin.value === senhaPadrao) {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
