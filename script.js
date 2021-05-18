3b34d6e0994cb16b6c529536a4feb42739b93ccb
gscuri <gscuri6@hotmail.com>
2021-05-18 17:31:33 -0300

    exercicio 3 com lint corrigido

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let selectButton = document.querySelector(".botaoSubmit");
-let selectLogin = document.querySelector("#login");
-let selectPassword = document.querySelector("#senha");
+const selectButton = document.querySelector('.botaoSubmit');
+const selectLogin = document.querySelector('#login');
+const selectPassword = document.querySelector('#senha');
 
-selectButton.addEventListener("click", function () {
+selectButton.addEventListener('click', function () {
   if (
-    selectLogin.value === "tryber@teste.com" &&
-    selectPassword.value === "123456"
+    selectLogin.value === 'tryber@teste.com' &&
+    selectPassword.value === '123456'
   ) {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   } else {
-    alert("Login ou senha inválidos.");
+    alert('Login ou senha inválidos.');
   }
 });
