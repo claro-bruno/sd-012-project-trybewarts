ff1ba1bf1d9f8ae61167b566c281088a1d3a2368
Ramon Paschoal <rpaschoal90@gmail.com>
2021-05-18 16:29:09 -0300

    Acertando lint

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-let botao = document.querySelector('#botao-login');
+const botao = document.querySelector('#botao-login');
 
-function preventD () {
-    botao.addEventListener('click', (event) => {
-        event.preventDefault();
-        botaoLoginPassword ()
-        })
+function botaoLoginPassword() {
+  let loginTrybewarts = document.querySelector('#login');
+  let senhaTrybewarts = document.querySelector('#password');
+  
+  if (loginTrybewarts.value !== 'tryber@teste.com' || senhaTrybewarts.value !== '123456') {
+    alert ('Login ou senha inválidos.');
+    }
+  if (loginTrybewarts.value === 'tryber@teste.com' && senhaTrybewarts.value === '123456') {
+    alert ('Olá, Tryber!');
+    }
+}
+
+function preventD() {
+  botao.addEventListener('click', (event) => {
+    event.preventDefault();
+    botaoLoginPassword();
+  })
 }
 
 preventD();
-
-function botaoLoginPassword () {
-    let loginTrybewarts = document.querySelector('#login');
-    let senhaTrybewarts = document.querySelector('#password');
-
-    if (loginTrybewarts.value !== 'tryber@teste.com' || senhaTrybewarts.value !== '123456') {
-        alert ('Login ou senha inválidos.');
-    }
-    if (loginTrybewarts.value === 'tryber@teste.com' && senhaTrybewarts.value === '123456') {
-        alert ('Olá, Tryber!');
-    }
-
-}
