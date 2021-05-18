9f7b376d94aafa6b9e30f1fd6a38a0cf93a49ac6
Daniel <danielbped@gmail.com>
2021-05-18 20:02:42 -0300

    Correçoes realizadas

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-const loginButton = document.querySelector('#login-button');
-const loginInput = document.querySelector('#login-input');
-const passInput = document.querySelector('#pass-input');
-const escolheCasa = document.querySelector('#house');
-const nota = document.querySelector('#rate');
-const checkboxAgreement = document.querySelector('#agreement');
-const submitButton = document.querySelector('#submit-btn');
+const loginButton = document.querySelector("#login-button");
+const loginInput = document.querySelector("#login-input");
+const passInput = document.querySelector("#pass-input");
+const escolheCasa = document.querySelector("#house");
+const nota = document.querySelector("#rate");
+const checkboxAgreement = document.querySelector("#agreement");
+const submitButton = document.querySelector("#submit-btn");
 
-loginButton.addEventListener('click', (event) => {
+loginButton.addEventListener("click", (event) => {
   event.preventDefault();
-  if (loginInput.value !== 'tryber@teste.com' || passInput.value !== '123456') {
-    alert('Login ou senha inválidos.');
+  if (loginInput.value !== "tryber@teste.com" || passInput.value !== "123456") {
+    alert("Login ou senha inválidos.");
   } else {
-    alert('Olá, Tryber!');
-    loginInput.value = '';
-    passInput.value = '';
+    alert("Olá, Tryber!");
+    loginInput.value = "";
+    passInput.value = "";
   }
 });
 
 function escolherCasa() {
   const casas = [
-    { casa: 'Gitnória', id: 'gitnoria-house' },
-    { casa: 'Reactpuff', id: 'reactpuff-house' },
-    { casa: 'Corvinode', id: 'corvinode-house' },
-    { casa: 'Pytherina', id: 'pytherina-house' },
+    { casa: "Gitnória", id: "gitnoria-house" },
+    { casa: "Reactpuff", id: "reactpuff-house" },
+    { casa: "Corvinode", id: "corvinode-house" },
+    { casa: "Pytherina", id: "pytherina-house" },
   ];
 
   for (let index = 0; index < casas.length; index += 1) {
-    const novaCasa = document.createElement('option');
+    const novaCasa = document.createElement("option");
     novaCasa.innerHTML = casas[index].casa;
     novaCasa.id = casas[index].id;
     escolheCasa.appendChild(novaCasa);
@@ -35,11 +35,11 @@ function escolherCasa() {
 
 function rate() {
   for (let index = 1; index <= 10; index += 1) {
-    const notaLabel = document.createElement('label');
+    const notaLabel = document.createElement("label");
     notaLabel.innerHTML = index;
-    const novaNota = document.createElement('input');
-    novaNota.name = 'rate';
-    novaNota.type = 'radio';
+    const novaNota = document.createElement("input");
+    novaNota.name = "rate";
+    novaNota.type = "radio";
     novaNota.value = index;
     nota.append(novaNota);
     nota.appendChild(notaLabel);
