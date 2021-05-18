4101c5d8f452db066a3d6d53b40fc07f0ebef01d
Netosilv4 <netosilv44445@gmail.com>
2021-05-18 20:31:23 -0300

    "Todos os requisitos"

--- script.js
+++ script.js
@@ -30,37 +30,25 @@ textarea.addEventListener('input', () => {
   counter.innerText = 500 - textarea.value.length;
 });
 
-const form = document.querySelector('#evaluation-form')
-const p = document.createElement('div')
-buttonEnviar.addEventListener('click', () => {
-    const nomeCompleto = `<p>Nome: ${document.querySelector('#input-name').value} ${document.querySelector('#input-lastname').value}</p>`
-    const email = `<p>Email: ${document.querySelector('#input-email').value}@mail.com</p>`
-    const casa = `<p>Casa: ${document.querySelector('#house').value}</p>`
-    const familia = `<p>Família: ${document.querySelector('input[name="family"]:checked').value}</p>`
-    let materias = 'Matérias: '
-    const materia = document.querySelectorAll('input[class="subject"]:checked')
-    materia.forEach(element => {
-      materias += `${element.value}, `
-    });
-    const avaliacao = `<p>Avaliação: ${document.querySelector('input[name="rate"]:checked').value}</p>`
-    const observacoes = `<p>Observações: ${document.getElementById('textarea').value}`
-    form.innerHTML = '';
-    p.innerHTML = nomeCompleto
-    p.innerHTML += email
-    p.innerHTML += casa
-    p.innerHTML += familia
-    p.innerHTML += materias
-    p.innerHTML += avaliacao
-    p.innerHTML += observacoes
-    form.appendChild(p)
-})
+const form = document.querySelector('#evaluation-form');
 
+function createDiv(dataForm) {
+  form.innerHTML = '';
+  console.log(dataForm);
+  form.innerHTML = `<p>Nome: ${dataForm.nome} ${dataForm.sobrenome}</p>`;
+  form.innerHTML += `<p>Email: ${dataForm.email}</p>`;
+  form.innerHTML += `<p>Casa:  ${dataForm.casa}</p>`;
+  form.innerHTML += `<p>Família: ${dataForm.family}</p>`;
+  form.innerHTML += `<p>Matérias: ${dataForm.subject.toLocaleString().replaceAll(',', ', ')}</p>`;
+  form.innerHTML += `<p>Avaliação: ${dataForm.rate}</p>`;
+  form.innerHTML += `<p>Observações: ${dataForm.comentario}</p>`;
+}
 
-// Todos os campos do formulário devem ser substituídos.
-// Deve haver um campo com o formato "Nome: Alguem Aqui"
-// Deve haver um campo com o formato "Email: email@mail.com"
-// Deve haver um campo com o formato "Casa: Casa Escolhida"
-// Deve haver um campo com o formato "Família: Família Escolhida"
-// Deve haver um campo com o formato "Matérias: Matérias, Marcadas, Aqui"
-// Deve haver um campo com o formato "Avaliação: NotaAqui"
-// Deve haver um campo com o formato "Observações: Observações aqui"
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const formJSON = Object.fromEntries(data.entries());
+  formJSON.subject = data.getAll('subject');
+  createDiv(formJSON);
+}
+form.addEventListener('submit', handleFormSubmit);
