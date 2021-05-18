7400f1dd561e4206a03c45ebf3ec3620088044eb
Wadson <wadson.dev92@gmail.com>
2021-05-18 18:39:30 -0300

    retira a funçao de dentro do es ecutadoor do eventos

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const login = document.getElementById('login');
 const senha = document.getElementById('senha');
 const botao = document.getElementById('btn');
 
-botao.addEventListener('click', function () {
+function alerta() {
   if (login.value !== 'tryber@teste.com' || senha.value !== '123456') {
     alert('Login ou senha inválidos');
   } else {
     alert('Olá, Tryber!');
   }
-});
+}
+botao.addEventListener('click', alerta);
