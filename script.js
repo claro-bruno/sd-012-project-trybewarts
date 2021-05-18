dc1defdfa52df659424f634fed478a71d7a2acbd
martinsph <ph.mmartins@gmail.com>
2021-05-18 19:45:30 -0300

    testando requsito 21

--- script.js
+++ script.js
@@ -31,3 +31,54 @@ const pCounter = document.querySelector('#counter');
 textArea.addEventListener('input', () => {
   pCounter.innerText = 500 - textArea.value.length;
 });
+
+const printForm = document.querySelector("#print-form-container");
+
+submitBtn.addEventListener('click', () => {
+  const name = document.querySelector('#input-name').value;
+  const lastName = document.querySelector('#input-lastname').value;
+  const email = document.querySelector('#input-email').value;
+  const house = document.querySelector('#house option:selected');
+  const inputFamily = document.getElementsByName('family');
+  let familyValue = "";
+  const materia = document.getElementsByClassName('subject');
+  let materiaSelecionadas = "";
+  const avalicacao = document.getElementsByName('rate');
+  let avaliacaoValue = "";
+  const observacao = document.querySelector('#textarea').value;
+
+  for(let index =0; index < inputFamily.length; index += 1) {
+    if (inputFamily[index].checked) {
+      familyValue = inputFamily[index].value;
+    };
+  };
+  
+  for(let index =0; index < materia.length; index += 1) {
+    if (materia[index] === true) {
+      materiaSelecionadas += ` ${materia[index].value}`;
+    }
+  }
+
+  for(let index =0; index < avalicacao.length; index += 1) {
+    if (avalicacao[index].checked) {
+      avaliacaoValue = avalicacao[index].value;
+    };
+  };
+
+
+
+  // const form = {
+  //   nome: `${name} ${lastName}`,
+  //   email: `${email}`,
+  //   casa: `${house}`,
+  //   familia: "",
+  //   materias: "",
+  //   avaliacao: "",
+  //   observacoes: ""
+  // }
+
+  const addP = document.createElement('p');
+  printForm.appendChild(addP);
+  addP.innerText = `nome: ${name} ${lastName} \n email: ${email} \n casa: ${house} \n familia: ${familyValue} \n matérias:${materiaSelecionadas} \n avaliação: ${avaliacaoValue} \n observações ${observacao}`; 
+  console.log("aqui");
+});
