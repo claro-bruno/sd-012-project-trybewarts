2604d480e551e46e177615aa1000671f03badff0
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-05-18 14:29:47 -0300

    Refatoração requisitos 1 e 2

---
+++ script.js
@@ -0,0 +1,15 @@
+const loginBtn = document.querySelector('form button');
+
+function validateInput() {
+  const emailInput = document.querySelector('input[type="email"]');
+  const passwordInput = document.querySelector('input[type="password"]');
+  if (emailInput.value === 'tryber@teste.com'
+  && passwordInput.value === '123456') {
+    return alert('Olá, Tryber!');
+  }
+  return alert('Login ou senha inválidos.');
+}
+
+window.onload = () => {
+  loginBtn.addEventListener('click', validateInput);
+};
