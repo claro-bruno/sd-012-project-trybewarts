4ede770538b886010bad0d6ec6175b0d8e00593d
Renata Nunes <renatanunes231@yahoo.com.br>
2021-05-18 20:44:12 -0300

    Ajustes Lint

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function alerta() {
-  const email = document.getElementById("email");
-  const senha = document.getElementById("senha");
-  const botaoLogar = document.getElementById("logar");
-  botaoLogar.addEventListener("click", () => {
-    if (email.value !== "tryber@teste.com" || senha.value !== "123456") {
-      alert("Login ou senha inválidos.");
+  const email = document.getElementById('email');
+  const senha = document.getElementById('senha');
+  const botaoLogar = document.getElementById('logar');
+  botaoLogar.addEventListener('click', () => {
+    if (email.value !== 'tryber@teste.com' || senha.value !== '123456') {
+      alert('Login ou senha inválidos.');
     } else {
-      alert("Olá, Tryber!");
+      alert('Olá, Tryber!');
     }
   });
 }
 
 function submit() {
-  const concorda = document.getElementById("agreement");
-  const botaoEnviar = document.getElementById("submit-btn");
-  concorda.addEventListener("click", () => {
+  const concorda = document.getElementById('agreement');
+  const botaoEnviar = document.getElementById('submit-btn');
+  concorda.addEventListener('click', () => {
     if (concorda.checked === true) {
       botaoEnviar.disabled = false;
     } else {
