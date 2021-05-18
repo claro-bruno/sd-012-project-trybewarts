efa1ab96dab084401ea956235ae475e66238b3b8
Milton Castro <castro.milton07@gmail.com>
2021-05-18 16:19:16 -0300

    Requisito 3 - Completo

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const btnLogar = document.getElementById('btnLogar');
+const loginForm = document.querySelector('.trybewarts-login');
+
+function getFormData(data) {
+  const form = {
+    login: data.elements.login.value,
+    password: data.elements.password.value,
+  };
+  return form;
+}
+
+function verifyForm(formData) {
+  if ((formData.login === 'tryber@teste.com') && (formData.password === '123456')) {
+    return true;
+  } return false;
+}
+
+function validateLogin(event) {
+  event.preventDefault();
+  const form = event.target;
+  const formData = getFormData(form);
+  if (verifyForm(formData)) {
+    alert('Olá, Tryber!');
+  } else alert('Login ou senha inválidos.');
+}
+loginForm.addEventListener('submit', validateLogin);
