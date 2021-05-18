298fc7ef3e350b417edc1491a6c8256f62c2c337
alestori <alestori7@gmail.com>
2021-05-18 16:23:55 -0300

    refatora função preventDef

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const password = document.querySelector('.password');
 const login = document.querySelector('.login');
 const loginButton = document.querySelector('.login-button');
 
-function preventDef(event) {
-  event.preventDefault();
-}
+const preventDef = (event) => event.preventDefault();
 
 loginButton.addEventListener('click', () => {
   preventDef();
