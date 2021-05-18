a402ab64bc9ce76642ddbc96e3a98f94bcc6b7ae
alestori <alestori7@gmail.com>
2021-05-18 16:26:48 -0300

    refatoração do js

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const loginButton = document.querySelector('.login-button');
 
 const preventDef = (event) => event.preventDefault();
 
+loginButton.addEventListener('click', preventDef);
+
 loginButton.addEventListener('click', () => {
-  preventDef();
   if (login.value !== 'tryber@teste.com' || password.value !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
