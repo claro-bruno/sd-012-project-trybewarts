b14ec91dcea68515e23aeaae7128384115defcf1
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-05-18 20:12:54 -0300

    revisão lint script js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const loginButton = document.getElementById('botao-login');
 
 function checkLogin() {
-  let loginField = document.getElementById("login-field");
-  let senha = document.getElementById("senha");
+  const loginField = document.getElementById('login-field');
+  const senha = document.getElementById('senha');
   (loginField.value.toString() === 'tryber@teste.com') && (senha.value.toString() === '123456')
     ? alert('Olá, Tryber!')
     : alert('Login ou senha inválidos.');
