30535d68152e0cb1bcccae0e8113aaee235e4cc6
Marcela Silva <marcela.ops@hotmail.br>
2021-05-18 17:58:00 -0300

    r3 formulário login e senha

---
+++ script.js
@@ -0,0 +1,12 @@
+const inputButton = document.getElementById('inputButton');
+
+function validateLogin() {
+  const login = document.getElementById('login').value;
+  const senha = document.getElementById('senha').value;
+  if (login === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+inputButton.addEventListener('click', validateLogin);
