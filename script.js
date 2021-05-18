a3c15d4042da28d0b75e6b0a2d8e87483dc2034e
Adryan Rosa <adryan.lmrosa@gmail.com>
2021-05-18 16:05:53 -0300

    Satisfaz requisito 3

---
+++ script.js
@@ -0,0 +1,15 @@
+const emailLogin = document.getElementById("email-login");
+const passwordLogin = document.getElementById("password-login");
+const formLogin = document.getElementsByClassName("trybewarts-login");
+formLogin[0].addEventListener("submit", login);
+
+function login() {
+  if (
+    emailLogin.value === "tryber@teste.com" &&
+    passwordLogin.value === "123456"
+  ) {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Login ou senha inválidos.");
+  }
+}
