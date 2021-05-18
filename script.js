91d18e9917e9244f882c1f3985a58babd7e7c226
jmlinger <jmlinger10@gmail.com>
2021-05-18 20:05:00 -0300

    resolve requisito 18

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const inputLogin = document.getElementById('input-login');
 const inputSenha = document.getElementById('input-senha');
 const botaoLogar = document.getElementById('botao-logar');
+const botaoEnviar = document.getElementById('submit-btn');
 
-function loginAlert() {
+botaoLogar.addEventListener('click', () => {
   if (inputLogin.value === 'tryber@teste.com' && inputSenha.value === '123456') {
     alert('Olá, Tryber!');
   } if (inputLogin.value !== 'tryber@teste.com' || inputSenha.value !== '123456') {
     alert('Login ou senha inválidos.');
   }
-}
-botaoLogar.addEventListener('click', loginAlert);
+});
+
+botaoEnviar.disabled = true;
+botaoEnviar.addEventListener('input', () => {
+  botaoEnviar;
+  if (document.querySelectorAll('.subject:checked').length !== 0) {
+    botaoEnviar.disabled = false;
+  } if (document.querySelectorAll('.subject:checked').length === 0) {
+    botaoEnviar.disabled = true;
+  }
+});
