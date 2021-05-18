b48fe97028bf176b45d013bb989d22adb4b3f9f4
Renata Nunes <renatanunes231@yahoo.com.br>
2021-05-18 20:38:41 -0300

    Requisitos 18 - Concluido

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 function alerta() {
-  const email = document.getElementById('email');
-  const senha = document.getElementById('senha');
-  const botaoLogar = document.getElementById('logar');
-  botaoLogar.addEventListener('click', () => {
-    if (email.value !== 'tryber@teste.com' || senha.value !== '123456') {
-      alert('Login ou senha inválidos.');
+  const email = document.getElementById("email");
+  const senha = document.getElementById("senha");
+  const botaoLogar = document.getElementById("logar");
+  botaoLogar.addEventListener("click", () => {
+    if (email.value !== "tryber@teste.com" || senha.value !== "123456") {
+      alert("Login ou senha inválidos.");
     } else {
-      alert('Olá, Tryber!');
+      alert("Olá, Tryber!");
+    }
+  });
+}
+
+function submit() {
+  const concorda = document.getElementById("agreement");
+  const botaoEnviar = document.getElementById("submit-btn");
+  concorda.addEventListener("click", () => {
+    if (concorda.checked === true) {
+      botaoEnviar.disabled = false;
+    } else {
+      botaoEnviar.disabled = true;
     }
   });
 }
 
 window.onload = function load() {
   alerta();
+  submit();
 };
