f5a84cd8235cd9b529c13a349fea4d6b522e846f
Renata Nunes <renatanunes231@yahoo.com.br>
2021-05-18 17:23:48 -0300

    Resolvendo Lint novamente

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function alerta() {
-  const email = document.getElementById("email");
-  const senha = document.getElementById("senha");
-  const botaoLogar = document.getElementById("logar");
-  botaoLogar.addEventListener("click", () => {
-    if (email.value !== "tryber@teste.com" || senha.value !== "123456") {
-      alert("Login ou senha inválidos.");
+  const email = document.getElementById('email');
+  const senha = document.getElementById('senha');
+  const botaoLogar = document.getElementById('logar');
+  botaoLogar.addEventListener('click', () => {
+    if (email.value !== 'tryber@teste.com' || senha.value !== '123456') {
+      alert('Login ou senha inválidos.');
     } else {
-      alert("Olá, Tryber!");
+      alert('Olá, Tryber!');
     }
   });
 }
