c6c09ada9138f4b5559f73f9621dbc57d3e555cf
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-05-18 19:26:58 -0300

    envia requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputLogin = document.querySelector('#input-login');
 const inputPass = document.querySelector('#input-pass');
 const btnLogin = document.querySelector('.submit-btn-login');
+const checkAgree = document.querySelector('#agreement');
+const btnSubmit = document.querySelector('#submit-btn');
 
 function alertLogin() {
   if (inputLogin.value === 'tryber@teste.com' && inputPass.value === '123456') {
@@ -10,3 +12,13 @@ function alertLogin() {
   }
 }
 btnLogin.addEventListener('click', alertLogin);
+
+btnSubmit.disabled = true;
+function checkSelected() {
+  if (checkAgree.checked === true) {
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
+  }
+}
+checkAgree.addEventListener('click', checkSelected);
