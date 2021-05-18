65e05401f746eb926340b1cfea81e98d59310e94
Luan Martins Lorenzato <lorenzatoluan15@outlook.com>
2021-05-18 18:34:30 -0300

    Refatora codigo

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 const loginid = document.getElementById("login");
 const passwId = document.getElementById("password");
-l
 
 function Startlogin() {
     if (loginid.value === 'tryber@teste.com' && passwId.value === '123456') {
-
         alert("Olá, Tryber !")
     }
-
     else {
-
         alert("Login ou Senha inválidos.");
-
     }
 }
