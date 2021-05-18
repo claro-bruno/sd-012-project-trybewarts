02c6021fcafab0611522a5ba12e774a11e9c6db8
Caio César <caiovg32@gmail.com>
2021-05-18 16:56:23 -0300

    Req.3

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-const email = document.getElementById('email');
-const senha = document.getElementById('senha');
-const botaoLogar = document.getElementById('logar');
-
-botaoLogar.addEventListener('click', alerta());
 
 function alerta() {
+  const email = document.getElementById('email');
+  const senha = document.getElementById('senha');
+  const botaoLogar = document.getElementById('logar');
+
+  botaoLogar.addEventListener('click', () => {
   if (email.value !== 'tryber@teste.com' || senha.value !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
   }
+ });
 }
+
+window.onload = function load () {
+  alerta()
+};
