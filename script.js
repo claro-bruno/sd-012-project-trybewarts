cebe10364f52e1574d0e1a49f2f8c0746e2a6d9a
vinicius-patriota <vinicius.patriota@outlook.com.br>
2021-05-18 17:37:07 -0300

    Requisitos 13 e 17

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-window.onload = function() {
+window.onload = function () {
     let btn = document.querySelector('.btn-submit');
-    btn.addEventListener('click', function() {
+    btn.addEventListener('click', function () {
         let login = document.querySelector('.input-login');
         let loginValue = login.value;
         let password = document.querySelector('.input-senha');
         let passwordValue = password.value;
-        if(loginValue == 'tryber@teste.com' && passwordValue == 123456){
+        if (loginValue == 'tryber@teste.com' && passwordValue == 123456) {
             alert('Olá, Tryber!');
         } else {
             alert('Login ou senha inválidos.');
