4b2154f0cad9138baf0c8bbce37151e583193149
Pedro Gonçalves <peg.gep27@gmail.com>
2021-05-18 16:00:47 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 window.onload = function () {
-<<<<<<< HEAD
-  let buttonLogin = document.getElementById('btn-login');
-  buttonLogin.addEventListener('click', logar);
-  let login = document.getElementById('login');
-  let senha = document.getElementById('senha');
-
-  function logar () {
-=======
   const buttonLogin = document.getElementById('btn-login');
   const login = document.getElementById('login');
   const senha = document.getElementById('senha');
 
   function logar() {
->>>>>>> e4b1a3f484db2e0b416472abb7b24790a230d995
     if (login.value === 'tryber@teste.com' && senha.value === '123456') {
       alert('Olá, Tryber!');
     }else {
       alert('Login ou senha inválidos.');
     }
   }
-<<<<<<< HEAD
-=======
   buttonLogin.addEventListener('click', logar);
->>>>>>> e4b1a3f484db2e0b416472abb7b24790a230d995
 };
