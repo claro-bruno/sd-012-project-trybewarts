b698a2261a855e9596ccb9563c60c7aac0e1306c
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-05-18 18:24:37 -0300

    conclui Desafio3

---
+++ script.js
@@ -0,0 +1,13 @@
+const login = document.getElementById('login');
+const senha = document.getElementById('senha');
+const botao = document.getElementById('botao');
+const loginText = 'tryber@teste.com';
+const senhaText = '123456';
+
+botao.addEventListener('click', () => {
+  if (login.value !== loginText || senha.value !== senhaText) {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+})
