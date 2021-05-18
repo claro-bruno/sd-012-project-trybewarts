717a13285e287d4be3d44d8039bcb2f283f7627a
Alexander <alexanderteodorocastro95@hotmail.com>
2021-05-18 19:29:24 -0300

    test 6

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function clicarLogar() {
   let email1 = document.getElementById('email').value.toLowerCase();
   let password2 = document.getElementById('password').value;
   if (email1 === 'tryber@teste.com' && password2 === '123456') {
-    alert('Olá, Tryber!');
+    alert("Olá, Tryber!");
   } else {
     alert('Login ou senha inválidos.');
     email1 = '';
