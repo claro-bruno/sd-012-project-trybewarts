a5c33dfd3b9b7ac3c132e2498f87abdc3e84ec03
lucasnahas <lucasahas@gmail.com>
2021-05-18 19:31:14 -0300

    adequa aos padroes do lint

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-
 // Requisito 3
-let loginSubmitBtn = document.getElementById('login-submit');
-let loginUser = document.getElementById('login-name');
-let loginPwd = document.getElementById('login-pwd');
-loginSubmitBtn.addEventListener('click', validateLogin);
-function validateLogin(e){
+const loginSubmitBtn = document.getElementById('login-submit');
+const loginUser = document.getElementById('login-name');
+const loginPwd = document.getElementById('login-pwd');
+function validateLogin(e) {
   e.preventDefault();
-  let user = loginUser.value;
-  let pwd = loginPwd.value;
-  if (user == 'tryber@teste.com' && pwd == '123456'){
+  const user = loginUser.value;
+  const pwd = loginPwd.value;
+  if (user === 'tryber@teste.com' && pwd === '123456') {
     alert('Olá, Tryber!');
-  }else{
+  } else {
     alert('Login ou senha inválidos.');
   }
 }
-
+loginSubmitBtn.addEventListener('click', validateLogin);
