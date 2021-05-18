38c044be4982501fe3459c60e6bc649fbf57bd8a
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-05-18 18:23:48 -0300

    verifica o lint

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 window.onload = function() {
-    let btn = document.querySelector('.btn-submit');
-    btn.addEventListener('click', function() {
-        let login = document.querySelector('.input-login');
-        let loginValue = login.value;
-        let password = document.querySelector('.input-senha');
-        let passwordValue = password.value;
-        if(loginValue == 'tryber@teste.com' && passwordValue == 123456){
-            alert('Olá, Tryber!');
-        } else {
-            alert('Login ou senha inválidos.');
-        }
-    })
+  const btn = document.querySelector('.btn-submit');
+  btn.addEventListener('click', function() {
+    const login = document.querySelector('.input-login');
+    const loginValue = login.value;
+    const loginTexto = loginValue.toString();
+    const password = document.querySelector('.input-senha');
+    const passwordValue = password.value;
+    const passwordSenha = parseInt(passwordValue);
+    if (loginTexto === 'tryber@teste.com' && passwordSenha === 123456) {
+        alert('Olá, Tryber!');
+    } else {
+        alert('Login ou senha inválidos.');
+    }
+  })
 }
