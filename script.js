e9288c79baceb90b9f8137b7314fe2b38270b069
Netosilv4 <netosilv44445@gmail.com>
2021-05-18 18:53:51 -0300

    Requisitos bonus

--- script.js
+++ script.js
@@ -29,3 +29,38 @@ const counter = document.getElementById('counter');
 textarea.addEventListener('input', () => {
   counter.innerText = 500 - textarea.value.length;
 });
+
+const form = document.querySelector('#evaluation-form')
+const p = document.createElement('div')
+buttonEnviar.addEventListener('click', () => {
+    const nomeCompleto = `<p>Nome: ${document.querySelector('#input-name').value} ${document.querySelector('#input-lastname').value}</p>`
+    const email = `<p>Email: ${document.querySelector('#input-email').value}@mail.com</p>`
+    const casa = `<p>Casa: ${document.querySelector('#house').value}</p>`
+    const familia = `<p>Família: ${document.querySelector('input[name="family"]:checked').value}</p>`
+    let materias = 'Matérias: '
+    const materia = document.querySelectorAll('input[class="subject"]:checked')
+    materia.forEach(element => {
+      materias += `${element.value}, `
+    });
+    const avaliacao = `<p>Avaliação: ${document.querySelector('input[name="rate"]:checked').value}</p>`
+    const observacoes = `<p>Observações: ${document.getElementById('textarea').value}`
+    form.innerHTML = '';
+    p.innerHTML = nomeCompleto
+    p.innerHTML += email
+    p.innerHTML += casa
+    p.innerHTML += familia
+    p.innerHTML += materias
+    p.innerHTML += avaliacao
+    p.innerHTML += observacoes
+    form.appendChild(p)
+})
+
+
+// Todos os campos do formulário devem ser substituídos.
+// Deve haver um campo com o formato "Nome: Alguem Aqui"
+// Deve haver um campo com o formato "Email: email@mail.com"
+// Deve haver um campo com o formato "Casa: Casa Escolhida"
+// Deve haver um campo com o formato "Família: Família Escolhida"
+// Deve haver um campo com o formato "Matérias: Matérias, Marcadas, Aqui"
+// Deve haver um campo com o formato "Avaliação: NotaAqui"
+// Deve haver um campo com o formato "Observações: Observações aqui"
