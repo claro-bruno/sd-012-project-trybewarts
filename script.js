80c213223f1a8ba2a2a1082dbe356776e66c1bf9
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-05-18 21:49:20 -0300

    Correções pequenas e Requisito 20

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+// Captura elementos
 const loginBtn = document.querySelector('#login-btn');
 const loginInput = document.querySelector('#login-input');
 const loginPass = document.querySelector('#login-pass');
+const agreementCheck = document.querySelector('#agreement');
+const submitBtn = document.querySelector('#submit-btn');
+submitBtn.disabled = true;
+const textArea = document.querySelector('#textarea-txt');
+const counter = document.querySelector('#counter');
 
+// Valida o login
 const validateLogin = () => loginInput.value === 'tryber@teste.com'
   && loginPass.value === '123456';
 
+// Adiciona evento ao botão submit
 loginBtn.addEventListener('click', () => {
   if (validateLogin()) {
     alert('Olá, Tryber!');
@@ -13,12 +21,17 @@ loginBtn.addEventListener('click', () => {
   }
 });
 
-const submitBtn = document.querySelector('#submit-btn');
-const agreementCheck = document.querySelector('#agreement');
-submitBtn.disabled = true;
-
+// Adiciona evento ao checkbox
 agreementCheck.addEventListener('click', () => {
   if (agreementCheck.checked) {
     submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
   }
+});
+
+// Atualiza contador on-the-fly
+textArea.addEventListener('input', (event) => {
+  const newLength = event.target.value.length;
+  counter.innerText = 500 - newLength;
 });
