47b563bf68df847eb2efeadc60ef877071d4ef6c
julio11lp <julio11lp@hotmail.com>
2021-05-19 14:59:05 -0300

    Criterio 21 capturando e exibindo textos

--- script.js
+++ script.js
@@ -24,16 +24,12 @@ function enableSubmit() {
   checkbox.addEventListener('click', () => {
     const condition = checkbox.value;
     if (condition === 'false') {
-      console.log(checkbox.value);
       submitButton.disabled = false;
       checkbox.value = 'true';
-      console.log(checkbox.value);
     }
     if (condition === 'true') {
-      console.log(checkbox.value);
       submitButton.disabled = true;
       checkbox.value = 'false';
-      console.log(checkbox.value);
     }
   });
 }
@@ -50,8 +46,79 @@ function countCharacters() {
     }
     const maxCharacters = 500;
     const remains = maxCharacters - typedLength;
-    console.log(remains);
     document.querySelector('#counter').innerHTML = remains;
   });
 }
 countCharacters();
+
+function displaySelected() {
+  const subjectsField = document.querySelector('#userSubject');
+  const inputSUbjects = document.getElementsByName('subject');
+  const selectedSubjects = [];
+  for (let index = 0; index < inputSUbjects.length; index += 1) {
+    if (inputSUbjects[index].checked) {
+      const selected = inputSUbjects[index].value;
+      selectedSubjects.push(selected);
+    }
+  }
+  const displaySubjects = `Matérias: ${selectedSubjects}`;
+  subjectsField.innerHTML = displaySubjects;
+}
+
+function displayName() {
+  const nameField = document.querySelector('#userFullName');
+  const inputName = document.querySelector('#input-name').value;
+  const inputLastName = document.querySelector('#input-lastname').value;
+  const displayfullName = `Nome: ${inputName} ${inputLastName}`;
+  nameField.innerHTML = displayfullName;
+}
+
+function displayEmail() {
+  const emailField = document.querySelector('#userEmail');
+  const inputEmail = document.querySelector('#input-email').value;
+  const displayUserEmail = `Email: ${inputEmail}`;
+  emailField.innerHTML = displayUserEmail;
+}
+
+function displayHouse() {
+  const houseField = document.querySelector('#userHouse');
+  const inputHouse = document.querySelector('#house').value;
+  const displayUserHouse = `Casa: ${inputHouse}`;
+  houseField.innerHTML = displayUserHouse;
+}
+
+function displayFamily() {
+  const familyField = document.querySelector('#userFamily');
+  const inputOption = document.querySelector('input[name="family"]:checked').value;
+  const displayOption = `Família: ${inputOption}`;
+  familyField.innerHTML = displayOption;
+}
+
+function displayRateChecked() {
+  const rateField = document.querySelector('#userRateChecked');
+  const inputRate = document.querySelector('input[name="rate"]:checked').value;
+  const displayRate = `Avaliação: ${inputRate}`;
+  rateField.innerHTML = displayRate;
+}
+
+function displayFeedback() {
+  const feedbackField = document.querySelector('#userFeedback');
+  const inputFeedback = document.querySelector('textarea').value;
+  const displayUserFeedback = `Observações: ${inputFeedback}`;
+  feedbackField.innerHTML = displayUserFeedback;
+}
+
+function submitInfos() {
+  const submitButton = document.querySelector('#submit-btn');
+
+  submitButton.addEventListener('click', () => {
+    displayName();
+    displayEmail();
+    displayHouse();
+    displayFamily();
+    displaySelected();
+    displayRateChecked();
+    displayFeedback();
+  });
+}
+submitInfos();
