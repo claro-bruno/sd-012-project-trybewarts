9e4b967753f5498889a7115cbcd2e5e61257518e
matheustkaczyk <matheus111904@hotmail.com>
2021-05-19 11:22:48 -0400

    obrigatorio parciais

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const emailLogin = document.getElementById('email-login');
 const senhaLogin = document.getElementById('senha-login');
 const submitLogin = document.querySelector('.submit-login');
 const formLogin = document.getElementById('form-login');
+const submitAll = document.getElementById('submit-btn');
+const checkbox = document.getElementById('agreement');
 const emailPadrao = "tryber@teste.com";
 const senhaPadrao = "123456"
 
@@ -19,3 +21,14 @@ function validaLogin() {
 }
 
 submitLogin.addEventListener('click', validaLogin);
+
+//exercicio feito com auxilio do repositorio do colega thales e matheus <3
+function disabled(){
+  if(!checkbox.checked){
+      submitAll.setAttribute('disabled',true);
+  } else {
+      submitAll.removeAttribute('disabled');
+  }
+}
+
+checkbox.addEventListener('click', disabled);
