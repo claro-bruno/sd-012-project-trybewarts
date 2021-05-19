97b1269ee806f16bd0ddf9410275c7eda912c92f
Alexander <alexanderteodorocastro95@hotmail.com>
2021-05-19 19:34:54 -0300

    requisito 8/9

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const pegabotao = document.querySelector('#botao');
 function clicarLogar() {
-  let email1 = document.getElementById('email').value.toLowerCase();
+  let email1 = document.getElementById('email').value;
   let password2 = document.getElementById('password').value;
   if (email1 === 'tryber@teste.com' && password2 === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
-    email1 = '';
-    password2 = '';
+
   }
 }
 
