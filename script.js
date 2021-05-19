1d6fb289c678a5e550f8fcf1c187a6a2ba66566b
Patricia Silvestre <patriciasilvestrea@gmail.com>
2021-05-19 11:05:39 -0300

    Começa requisito 9

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function validation() {
   }
 }
 login.addEventListener('click', validation);
+
+
+function createOptionsHouse() {
+  const house = document.getElementById('house');
+  const houseOptionsValue = ['Gitnória', 'Reactpuff', 'Corvinode', 'Pytherina'];
+  const houseOptionsId = ['gitnoria-house', 'reactpuff-house', 'corvinode-house', 'pytherina-house'];
+
+  for (let index = 0; index < houseOptionsValue.length; index += 1) {
+    let createOption = houseOptionsValue[index];
+    let createOption2 = houseOptionsId[index];
+    createOption = document.createElement('option');
+    createOption2 = document.createElement('option');
+    house.appendChild(createOption).innerText = createOption;
+    house.appendChild(createOption).value = createOption;
+    house.appendChild(createOption2).innerText = createOption2;
+    house.appendChild(createOption2).value = createOption2;
+  }
+} house.addEventListener('click', createOptionsHouse);
