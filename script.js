eb50f66bb3d906fc14330b02278fc2dc82eecf3e
Josimar Souza <contact.josimarsouza@gmail.com>
2021-05-19 09:33:37 -0300

    Refatora de acordo com o lint

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let loginButton = document.getElementById('login-button');
-let emailInput = document.getElementById('trybewarts-email');
-let passwordInput = document.getElementById('trybewarts-senha');
-
-loginButton.addEventListener('click', verifyLogin);
+const loginButton = document.getElementById('login-button');
+const emailInput = document.getElementById('trybewarts-email');
+const passwordInput = document.getElementById('trybewarts-senha');
 
 function verifyLogin(event) {
-    console.log(emailInput.value);
-    if (emailInput.value !== 'tryber@teste.com' || passwordInput.value !== '123456') {
-        event.preventDefault();
-    } else {
-        console.log('Login feito');
-    }
+  console.log(emailInput.value);
+  if (emailInput.value !== 'tryber@teste.com' || passwordInput.value !== '123456') {
+    event.preventDefault();
+  } else {
+    console.log('Login feito');
+  }
 }
+
+loginButton.addEventListener('click', verifyLogin);
