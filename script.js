f857450415c32312e51a3326aa8b1715fc079baa
rachidb5 <jordan.rachid@gmail.com>
2021-05-19 00:23:42 -0300

    adiciona funçao de validaçao

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const loginField = document.getElementById('login-field');
 const senhaField = document.getElementById('senha-field');
 
 function validate() {
-    console.log(loginField.value)
   if (loginField.value === 'tryber@teste.com' && senhaField.value === '123456') {
     alert('Olá, Tryber!');
   } else {
@@ -10,4 +9,4 @@ function validate() {
   }
 }
 
-
+validate();
