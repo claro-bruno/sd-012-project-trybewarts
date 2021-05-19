e8d279fb34534eb47a0782999b5ecb50f0f34f98
Henrique Remedios <henriqueremedios@gmail.com>
2021-05-19 18:37:28 -0300

    Organizando CSS, add function no JS e add class nas div(s) do HTML

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const loginid = document.getElementById("login");
+const loginId = document.getElementById("login");
 const passwId = document.getElementById("password");
+const sendButton = document.querySelector('#submit-btn');
+const currentText = document.querySelector('#counter');
+const textArea = document.querySelector('#textarea');
 
 function Startlogin() {
-    if (loginid.value === 'tryber@teste.com' && passwId.value === '123456') {
-        alert("Olá, Tryber !")
+    if (loginId.value === 'tryber@teste.com' && passwId.value === '123456') {
+        alert("Olá, Tryber!")
     }
     else {
         alert("Login ou Senha inválidos.");
     }
 };
+
+function checkTerms() {
+    if (terms.checked) {
+      sendButton.disabled = false;
+    } else { sendButton.disabled = true; }
+};
+checkTerms();
+
+function characterAccount() {
+    let current = currentText.innerHTML;
+    current = 500;
+    currentText.innerHTML = current - textArea.value.length;
+};
+characterAccount();
