0d8cc37fc41c0fb6efd1af6d935905893ae051d2
Eric Kreis <erickreis793@gmail.com>
2021-05-18 22:24:31 -0300

    Completa requisitos totais :rocket:

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const loginButton = document.getElementById('button-login');
 const inputLogin = document.getElementById('input-login');
+const form = document.getElementById('evaluation-form');
+const firstName = document.getElementById('input-name');
+const lastName = document.getElementById('input-lastname');
+const inputEmail = document.getElementById('input-email');
+const selectHouse = document.querySelectorAll('option');
+const familyRadio = document.getElementsByName('family');
+const rateRadio = document.getElementsByName('rate');
 const agreement = document.getElementById('agreement');
 const submitButton = document.getElementById('submit-btn');
 const textarea = document.getElementById('textarea');
@@ -16,7 +23,7 @@ function loginButtonExec() {
   });
 }
 
-function submitButtonExec() {
+function submitButtonEnable() {
   agreement.addEventListener('click', () => {
     if (agreement.checked) {
       submitButton.disabled = false;
@@ -34,6 +41,62 @@ function textareaExec() {
   });
 }
 
+function findSelected() {
+  for (let index = 0; index <= selectHouse.length; index += 1) {
+    if (selectHouse[index].selected) {
+      return selectHouse[index].innerHTML;
+    }
+  }
+}
+
+function findChecked(element) {
+  for (let index = 0; index <= element.length; index += 1) {
+    if (element[index].checked) {
+      return element[index].value;
+    }
+  }
+}
+
+function findSubjectsChecked() {
+  const subjectBoxes = document.querySelectorAll('.subject:checked');
+  const values = [];
+  subjectBoxes.forEach((checkbox) => {
+    values.push(checkbox.value);
+  });
+  return values.join(', ');
+}
+
+function createNewElement(string) {
+  const p = document.createElement('p');
+  p.innerHTML = string;
+  form.appendChild(p);
+}
+
+function changeInformation() {
+  const name = `Nome: ${firstName.value} ${lastName.value}`;
+  const email = `Email: ${inputEmail.value}`;
+  const house = `Casa: ${findSelected()}`;
+  const family = `Família: ${findChecked(familyRadio)}`;
+  const subject = `Matérias: ${findSubjectsChecked()}`;
+  const rate = `Avaliação: ${findChecked(rateRadio)}`;
+  const obs = `Observações: ${textarea.value}`;
+  form.innerHTML = '';
+  createNewElement(name);
+  createNewElement(email);
+  createNewElement(house);
+  createNewElement(family);
+  createNewElement(subject);
+  createNewElement(rate);
+  createNewElement(obs);
+}
+
+function submitButtonExec() {
+  submitButton.addEventListener('click', (event) => {
+    changeInformation();
+  });
+}
+
 loginButtonExec();
 textareaExec();
+submitButtonEnable();
 submitButtonExec();
