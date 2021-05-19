a8e62feb65fa0b93b5b663034224a6241b22c6e8
Samuel Constantino <samuelicapuidados@gmail.com>
2021-05-19 08:46:58 -0300

    refatora javascript e adiciona comentários

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
-const inputLogin = document.querySelector('#inputLogin');
 
-function botaoLogin(event) {
-  if (event.target.id === 'botaoLogin') {
-    const inputSenha = document.querySelector('#inputSenha');
-    if (inputLogin.value === 'tryber@teste.com' && inputSenha.value === '123456') {
-      alert('Olá, Tryber!');
-    } else {
-      alert('Login ou senha inválidos.');
-      event.preventDefault();
-    }
+// valida login e senha
+function validacaoLoginSenha(event) {
+  // acessa elemento com id 'inputSenha'
+  const inputSenha = document.querySelector('#inputSenha');
+  // acessa elemento com id 'inputLogin'
+  const inputLogin = document.querySelector('#inputLogin');
+  // se o valor do login e o valor da senha corresponderem aos valores definidos no requisito
+  if (inputLogin.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+    // mostra alerta de sucesso e envia formulário
+    alert('Olá, Tryber!');
+  } else {
+    // se não, mostra alerta de login/senha inválidos
+    alert('Login ou senha inválidos.');
+    // impede que o formulário seja enviado
+    event.preventDefault();
   }
 }
 
+//gerencia os clicks na página
 const listener = function (event) {
-  botaoLogin(event);
+
+  // se o alvo do click for o elemento com id 'botaoLogin'
+  if (event.target.id === 'botaoLogin') {
+    // valida login e senha
+    validacaoLoginSenha(event);
+  }
+  
+  // se o alvo do click for o elemento com id 'agreement' (checkbox)
   if (event.target.id === 'agreement') {
-    const botao = document.querySelector('#submit-btn');
+    // acessa elemento com id 'submit-btn'
+    const btnSubmit = document.querySelector('#submit-btn');
+    // se o checkbox estiver marcado
     if (event.target.value === 'on') {
-      botao.disabled = false;
+      // habilita botão de submit
+      btnSubmit.disabled = false;
+      // altera estado do checkbox para desmarcado
       event.target.value = 'off';
     } else {
-      botao.disabled = true;
+      // se não, desabilita botão submit
+      btnSubmit.disabled = true;
+      // altera estado do checkbox para marcado
       event.target.value = 'on';
     }
+
   }
 };
-
+// adiciona evento de click em toda a página
 document.addEventListener('click', listener);
