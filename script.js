3e15792f9b7e423e95c305a172c375ae48f179cc
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-05-18 21:14:42 -0300

    arquivo js e css: correçoes: texto "Ola, Tryber!" e display tipo flex no form

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ btnLogin.addEventListener('click', () => {
   if (inputLogin.value !== 'tryber@teste.com' && inputSenha.value !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
-    alert('Olá, Tryyber!');
+    alert('Olá, Tryber!');
   }
 });
