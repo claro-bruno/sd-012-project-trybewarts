7e40985dc25372178df26ae3591a5013b6bb3d20
camila-mp <ca.malvp@gmail.com>
2021-05-19 09:50:18 -0300

    inicia req. 21

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 const login = document.querySelector('#login');
 const senha = document.querySelector('#senha');
 const btn = document.querySelector('#buttonEntrar');
+const btn2 = document.querySelector('#submit-btn')
+
+
 
 function verificaLogin() {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
-  }
-}
+  };
+};
+
+document.getElementById('input-name');
+document.getElementById('input-lastname');
+document.getElementById('input-email');
+document.getElementById('house');
+
+// function capturaDados () {
+//   if (btn2.disabled === false) {
+
+//   }
+// }
 
 btn.addEventListener('click', verificaLogin);
 const valor = document.querySelector('#agreement');
 
 function habilitaBotão() {
-  const btn2 = document.querySelector('#submit-btn');
   if (valor.checked) {
     btn2.disabled = false;
   } else {
     btn2.disabled = true;
-  }
-}
+  };
+};
 
 valor.addEventListener('click', habilitaBotão);
+
+// btn2.addEventListener('click', substituiForm)
+
+
