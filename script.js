fa8bd0bae808f5b401daad74f0e4a34a231d0a3f
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-05-19 00:14:38 -0300

    questoes att

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const sendBtn = document.getElementById('submit-btn');
 const agreeCb = document.getElementById('agreement');
 const counter = document.getElementById('counter');
 const freeText = document.getElementById('textarea');
+const abstractPlace = document.querySelector('.abstract');
 
 sendBtn.disabled = true;
 
@@ -22,15 +23,40 @@ logarBtn.addEventListener('click', validaçãoLogin);
 
 function sendForm() {
   if (agreeCb.checked) { sendBtn.disabled = false; } else { sendBtn.disabled = true; }
+  generateText ();
 }
 
 agreeCb.addEventListener('click', sendForm);
 
 function countChar() {
   counter.innerText = `${500 - freeText.value.length}/500`;
-  console.log(counter.value);
 }
 
 freeText.addEventListener('keyup', countChar);
 
 window.onload = countChar;
+
+function generateElement(text) {
+  let textElement = document.createElement('p');
+  textElement.innerText = text;
+  abstractPlace.appendChild(textElement);
+}
+
+function generateText() {
+  const nameInput = document.getElementById('input-name').value;
+  const lastName = document.getElementById('input-lastname').value;
+  const email = document.getElementById('input-email').value;
+  const house = document.getElementById('house');
+  const family = document.getElementsByName('family');
+  const subject = document.getElementsByClassName('subject');
+  
+  generateElement(`Nome: ${nameInput} ${lastName}`);
+  generateElement(`Email: ${email}`);
+  generateElement(`Casa: ${house.options[house.selectedIndex].innerText}`);
+  
+  let checkedFamily = 0;
+  for(let index = 0; index < family.length; index += 1) {
+    if (family[index].checked === true) {checkedFamily = index; index = family.length;}
+  }
+  generateElement(`Família: ${family[checkedFamily].value}`);
+}
