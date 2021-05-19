0a3011daca774b108860a748e8620f769ff7d12c
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-05-19 09:22:15 -0300

    mudancas no codigo com requisito 3 resolvido

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ let senha = document.getElementById("input-senha");
 let botao = document.getElementById("input-botao");
 
 function mensagemFormulario(){
-    if (login.value === "tryber@teste.com" || senha.value === "123456"){
+    let login = document.getElementById("input-login");
+    let senha = document.getElementById("input-senha");
+        if (login.value === "tryber@teste.com" && senha.value === "123456"){
         alert("Olá, Tryber!")
-    }
-    else {
+         }
+        else {
         alert("Login ou senha inválidos.") 
-    };
+         };
 };
 botao.addEventListener("click", mensagemFormulario);
