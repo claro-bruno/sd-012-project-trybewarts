1cb0185396b019cd1a435e3aeb3206c65ad7a2eb
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-05-18 23:29:18 -0300

    Mudanças

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const botaoLogin = document.querySelector('#botao-login');
 const inputLogin = document.querySelector('#email');
 const inputSenha = document.querySelector('#senha');
+const habilitado = document.querySelector('#agreement');
+const botaoSubmit = document.querySelector('#submit-btn');
 
 function login() {
   if (inputLogin.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -11,4 +13,3 @@ function login() {
 }
 
 botaoLogin.addEventListener('click', login);
-
