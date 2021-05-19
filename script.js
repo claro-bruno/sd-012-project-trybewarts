f7e72050642a84dbfb1a165e7ab949b86b934b76
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-05-18 23:25:32 -0400

    Desafio 8 - Adiciona os campos nome, sobrenome e email ao forms

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function login() {
   const btLogin = document.querySelector('#btLogin');
   btLogin.addEventListener('click', () => {
-    const inputEmail = document.querySelector('#email');
-    const inputPasswd = document.querySelector('#passwd');
+    const inputEmail = document.querySelector('#email-login');
+    const inputPasswd = document.querySelector('#passwd-login');
     if (inputEmail.value === 'tryber@teste.com' && inputPasswd.value === '123456') {
       alert('Olá, Tryber!');
       inputEmail.value = '';
