a857ce5ac6d17d16f9837ffb218e2ca4cbc09e99
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-19 18:53:33 -0300

    corrige linter

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function enviar(event) {
-	const sendButton = document.getElementById('submit-btn');
-	if (event.target.value === 'on') {
-		sendButton.disabled = false;
-		event.target.value = 'off';
-	} else {
-		sendButton.disabled = true;
-		event.target.value = 'on'
-	}
+		const sendButton = document.getElementById('submit-btn');
+		if (event.target.value === 'on') {
+				sendButton.disabled = false;
+				event.target.value = 'off';
+		} else {
+				sendButton.disabled = true;
+				event.target.value = 'on';
+		}
 }
 
 function concorda(event) {
-	if (event.target.id === 'agreement') {
-		enviar(event);
-	} else {
+		if (event.target.id === 'agreement') {
+				enviar(event);
+		} else {
 		event.target.removeEventListener('click', concorda);
-	}
+		}
 }
 
 document.addEventListener('click', concorda);
