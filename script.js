e2f5dad1aa3b42c9cfaa77df931ea80032e0ed78
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-18 22:08:03 -0300

    prepara para requisito 21

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const botaoLogin = document.querySelector('#botao-login');
 const inputLogin = document.querySelector('#email');
 const inputSenha = document.querySelector('#senha');
+// const formulario = document.querySelector('#evaluation-form');
+// const botaoSubmit = document.querySelector('#submit-btn');
+// const inputNome = document.querySelector('#input-name');
+// const inputSobrenome = document.querySelector('#input-lastname');
+// const inputEmail = document.querySelector('#input-email');
+// const inputCasa = document.querySelector('#house');
+// let informacoesPreenchidas = {
+//   nome: '',
+//   email: '',
+//   casa: '',
+//   familia: '',
+//   materias: '',
+//   avaiacao: '',
+//   observacoes: ''
+// };
 
 function login() {
   if (inputLogin.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -11,3 +26,20 @@ function login() {
 }
 
 botaoLogin.addEventListener('click', login);
+
+// function removerConteudo() {
+//   const conteudoForm = formulario.children;
+//   for (let index = 0; index < conteudoForm.length; index += 1) {
+//     formulario.removeChild(conteudoForm[index]);
+//   }
+// }
+
+// function adicionarInformações() {
+//   informacoesPreenchidas.nome = inputNome.value + ' ' + inputSobrenome.value;
+//   informacoesPreenchidas.email = inputEmail.value;
+//   informacoesPreenchidas.casa = inputCasa.value;
+// }
+
+// botaoSubmit.addEventListener('click', adicionarInformações);
+
+// botaoSubmit.disabled = false;
