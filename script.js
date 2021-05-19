cab346a76226c25f7efe4ae2a3bcc8c63b5ab9a3
Kevin Oliveira <kevin.zero@hotmail.com>
2021-05-19 17:00:59 -0300

    Finaliza requisito 21

--- script.js
+++ script.js
@@ -25,81 +25,85 @@ textAreaInput.addEventListener('keyup', () => {
   textAreaInput.nextElementSibling.innerHTML = 500 - textAreaInput.value.length;
 });
 
-const firstName = document.querySelector('#input-name');
-const lastName = document.querySelector('#input-lastname');
-const email = document.querySelector('#input-email');
-const house = document.querySelector('#house');
-const comment = document.querySelector('#textarea');
-const evaluation = document.querySelector('.evaluation-note');
-
-function getRadioValue(radioClass) {
-  const radios = document.querySelectorAll(radioClass);
-  for (let index = 0; index < radios.length; index += 1) {
-    if (radios[index].checked) {
-      return radios[index].value;
-    }
-  }
-}
+const form = document.querySelector('#evaluation-form');
 
 function replaceNameElement() {
+  const fullName = document.querySelector('#full-name');
+  const firstName = document.querySelector('#input-name');
+  const lastName = document.querySelector('#input-lastname');
   const nameParagraph = document.createElement('p');
   nameParagraph.innerHTML = `Nome: ${firstName.value} ${lastName.value}`;
-  firstName.parentNode.removeChild(firstName.parentNode.lastElementChild);
-  firstName.parentNode.replaceChild(nameParagraph, firstName);
+  fullName.remove();
+  form.appendChild(nameParagraph);
 }
 
 function replaceEmailElement() {
+  const email = document.querySelector('#input-email');
   const emailParagraph = document.createElement('p');
   emailParagraph.innerHTML = `Email: ${email.value}`;
-  email.parentNode.replaceChild(emailParagraph, email);
+  email.remove();
+  form.appendChild(emailParagraph);
 }
 
 function replaceHouseElement() {
+  const house = document.querySelector('#house');
   const houseParagraph = document.createElement('p');
   houseParagraph.innerHTML = `Casa: ${house.value}`;
-  house.parentNode.replaceChild(houseParagraph, house);
-}
-
-function replaceRateElement() {
-  const replaceParagraph = document.createElement('p');
-  replaceParagraph.innerHTML = `Avaliação: ${getRadioValue('.eval')}`;
-  evaluation.parentNode.replaceChild(replaceParagraph, evaluation);
+  house.remove();
+  form.appendChild(houseParagraph);
 }
 
 function replaceFamilyElement() {
-  const family = document.querySelector('input[name="family"]:checked');
-  const familyCointainer = family.parentNode;
+  const family = document.querySelectorAll('input[name="family"]');
+  const familyContainer = document.querySelector('#family-container');
   const familyParagraph = document.createElement('p');
-  familyParagraph.innerHTML = `Família: ${family.value}`;
-  while (familyCointainer.firstChild) {
-    familyCointainer.removeChild(familyCointainer.firstChild);
+  for (let option = 0; option < family.length; option += 1) {
+    if (family[option].checked) {
+      familyParagraph.innerHTML = `Família: ${family[option].value}`;
+    }
   }
-  familyCointainer.appendChild(familyParagraph);
+  familyContainer.remove();
+  form.appendChild(familyParagraph);
 }
 
 function replaceContentElement() {
-  const content = document.querySelectorAll('input[class=subject]:checked');
-  const contentContainer = content[0].parentNode.parentNode;
+  const content = document.querySelectorAll('input[class=subject]');
+  const contentContainer = document.querySelector('#content-container');
   const contentParagraph = document.createElement('p');
   const checkedArray = [];
   for (let tech = 0; tech < content.length; tech += 1) {
-    checkedArray.push(content[tech].value);
-  }
-  while (contentContainer.firstChild) {
-    contentContainer.removeChild(contentContainer.firstChild);
+    if (content[tech].checked) {
+      checkedArray.push(content[tech].value);
+    }
   }
   contentParagraph.innerHTML = `Matérias: ${checkedArray.join(', ')}`;
-  contentContainer.appendChild(contentParagraph);
+  contentContainer.remove();
+  form.appendChild(contentParagraph);
+}
+
+function replaceRateElement() {
+  const rate = document.querySelectorAll('input[name="rate"]');
+  const evaluationContainer = document.querySelector('.evaluation-note');
+  const rateParagraph = document.createElement('p');
+  for (let option = 0; option < rate.length; option += 1) {
+    if (rate[option].checked) {
+      rateParagraph.innerHTML = `Avaliação: ${rate[option].value}`;
+    }
+  }
+  evaluationContainer.remove();
+  form.appendChild(rateParagraph);
 }
 
 function replaceCommentElement() {
+  const comment = document.querySelector('#textarea');
+  const commentContainer = document.querySelector('#textarea-container');
   const commentParagraph = document.createElement('p');
-  const commentContainer = comment.parentNode;
-  while (commentContainer.firstChild) {
-    commentContainer.removeChild(commentContainer.firstChild);
-  }
   commentParagraph.innerHTML = `Observações: ${comment.value} `;
-  commentContainer.appendChild(commentParagraph);
+  commentContainer.remove();
+  form.appendChild(commentParagraph);
+  form.removeChild(form.firstElementChild);
+  form.removeChild(form.firstElementChild);
+  form.removeChild(form.firstElementChild);
 }
 
 submitButton.addEventListener('click', () => {
