e6a24cb9652f8fb1698c9b03ed8dcdd09b9a713d
Luan Martins Lorenzato <lorenzatoluan15@outlook.com>
2021-05-19 20:08:31 -0300

    Refatora JS

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const loginId = document.getElementById("login");
-const passwId = document.getElementById("password");
+const loginId = document.getElementById('login');
+const passwId = document.getElementById('password');
 const sendButton = document.querySelector('#submit-btn');
 
-
 function Startlogin() {
-    if (loginId.value === 'tryber@teste.com' && passwId.value === '123456') {
-        alert("Olá, Tryber!")
-    }
-    else {
-        alert("Login ou Senha inválidos.");
-    }
-};
+    if (loginId.value === 'tryber@teste.com' && passwId.value === '123456')
+
+        alert('Olá, Tryber!')
+
+    else
+        alert('Login ou Senha inválidos.');
+}
 
 function checkTerms() {
-    if (terms.checked) {
+
+    if (terms.checked)
+
         sendButton.disabled = false;
-    } else {
+
+    else
+
         sendButton.disabled = true;
-    };
-};
+
+}
+
 checkTerms();
