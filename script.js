8cc54f0be95d6215ee449218b008e44e95c5d3c0
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-05-18 21:54:19 -0300

    ...ate a questao 19

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const loginName = document.getElementById('login');
 const loginSenha = document.getElementById('senha');
 const logarBtn = document.getElementById('logar');
+const sendBtn = document.getElementById('submit-btn');
+const agreeCb = document.getElementById('agreement');
+
+sendBtn.disabled = true;
 
 function validaçãoLogin(event) {
   console.log(loginName, loginSenha);
@@ -13,3 +17,9 @@ function validaçãoLogin(event) {
 }
 
 logarBtn.addEventListener('click', validaçãoLogin);
+
+function sendForm() {
+  if (agreeCb.checked) { sendBtn.disabled = false; } else { sendBtn.disabled = true; }
+}
+
+agreeCb.addEventListener('click', sendForm);
