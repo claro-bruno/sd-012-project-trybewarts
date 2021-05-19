e8230ea3feb130b4abff489d21ec15aa7d87b076
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-19 19:05:08 -0300

    linter 3

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ function enviar(event) {
   const sendButton = document.getElementById('submit-btn');
   if (event.target.value === 'on') {
     sendButton.disabled = false;
-		event.target.value = 'off';
-	} else {
-		sendButton.disabled = true;
-		event.target.value = 'on';
-	}
+    event.target.value = 'off';
+  } else {
+    sendButton.disabled = true;
+    event.target.value = 'on';
+  }
 }
 
 function concorda(event) {
   if (event.target.id === 'agreement') {
-		enviar(event);
-	} else {
-  	event.target.removeEventListener('click', concorda);
-	}
+    enviar(event);
+  } else {
+    event.target.removeEventListener('click', concorda);
+ }
 }
 
 document.addEventListener('click', concorda);
