25bfe0db8c353c7ed376f8d560aa751645d76449
Alice Gonçalves <galice9992@gmail.com>
2021-05-19 21:20:50 -0300

    altera e resolve requisito 3

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
-window.onload = function () {
-    document.body.addEventListener("click", function(event) {
-        let email = document.getElementById('input-login').value;
-        let senha = document.getElementById('input-senha').value;
-        if (event.target.Classname === 'login-submit') {
-            let count = 0;
-            let senhaArray = [];
-            for (let indexEmail = 0; indexEmail < email.length; indexEmail += 1) {
-                if (email[indexEmail] === '@' || email[indexEmail] === '.') {
-                    count += 1;
-                    console.log(count)
-                }
-            }
-                if (count >= 2) {
-                    let beforeArroba = email.split('@');
-                    let beforePoint = beforeArroba[1].split('.');
-                    if (beforeArroba[0].length < 3) {
-                        alert('Login ou senha inválidos');
-                    } else if (beforeArroba[1].length < 3) {
-                        alert('Login ou senha inválidos');
-                    } else if (beforePoint[1].length < 3) {
-                        alert('Login ou senha inválidos');
-                    }  
-                } else {
-                    alert ('Login ou senha inválidos');
-                }
-            for (let indexSenha = 0; indexSenha < senha.length; indexSenha += 1 ) {
-                if (parseInt(senha[indexSenha]) >= 0 || parseInt(senha[indexSenha]) <= 9 ) {
-                    senhaArray.push(parseInt(senha[indexSenha]));
-                }
-            } if (senhaArray.length <= 6) {
-                alert ('Login ou senha inválidos');
-            }
-            alert ('Olá, Tryber!')
-        }
-    }
-    )
-};
+const login = document.querySelector('.login-button');
+login.addEventListener('click', () => {
+  if (document.getElementById('input-login').value !== 'tryber@teste.com'
+  || document.getElementById('input-senha').value !== '123456') {
+    window.alert('Login ou senha inválidos.');
+  } else {
+    window.alert('Olá, Tryber!');
+  }
+});
