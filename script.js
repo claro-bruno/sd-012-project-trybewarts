d95798d91b3c5a0030206970a86bb0fc96cbf71c
Luan Martins Lorenzato <lorenzatoluan15@outlook.com>
2021-05-19 21:35:11 -0300

    Reuisito 18 e Refatora codigo

--- script.js
+++ script.js
@@ -2,24 +2,40 @@ const loginId = document.getElementById('login');
 const passwId = document.getElementById('password');
 const sendButton = document.querySelector('#submit-btn');
 
+
 function Startlogin() {
-    if (loginId.value === 'tryber@teste.com' && passwId.value === '123456')
-
+    if (loginId.value === 'tryber@teste.com' && passwId.value === '123456') {
         alert('Olá, Tryber!')
-
-    else
+    }
+    else {
         alert('Login ou Senha inválidos.');
+    }
 }
+
+const activeBtn = () => {
+
+    if (document.getElementById('agreement').checked) {
+        document.getElementById('submit-btn').removeAttribute('disabled');
+
+    } else {
+        document.getElementById('submit-btn').setAttribute('disabled', 'disabled');
+    }
+
+}
+
+const disableBtn = () => {
+
+}
+
 
 function checkTerms() {
 
-    if (terms.checked)
-
+    if (terms.checked) {
         sendButton.disabled = false;
-
-    else
-
+    }
+    else {
         sendButton.disabled = true;
+    }
 
 }
 
