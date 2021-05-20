35632416787e361e7bcb1d080216f09684b5000e
Luan Martins Lorenzato <lorenzatoluan15@outlook.com>
2021-05-20 18:35:51 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const loginId = document.getElementById('login');
 const passwId = document.getElementById('password');
 const btn = document.getElementById('buttonId');
 const formsBtn = document.getElementById('agreement');
+const textarea = document.getElementById('textarea');
+const counter = document.getElementById('counter')
+
 
 function starLogin() {
     if (loginId.value === 'tryber@teste.com' && passwId.value === '123456') {
@@ -13,13 +16,18 @@ function starLogin() {
 btn.addEventListener('click', starLogin);
 
 const activeBtn = () => {
-
     if (document.getElementById('agreement').checked) {
         document.getElementById('submit-btn').removeAttribute('disabled');
-
-    }
-    else {
+    } else {
         document.getElementById('submit-btn').setAttribute('disabled', 'disabled');
     }
 }
 formsBtn.addEventListener('click', activeBtn);
+
+const textLimitCounter = () => {
+    const maxLength = 500;
+    document.getElementById('textarea').setAttribute('maxlength', maxLength)
+
+    counter.innerHTML = 500 - textarea.value.length;
+}
+textarea.addEventListener('keyup', textLimitCounter);
