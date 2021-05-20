48671ddb507664c81ad62e8661b7ed934f11d016
Pedro Resck <pedroresck1@gmail.com>
2021-05-20 00:14:14 -0300

    Requisito 3: resolvido - adiciona forms login e senha no header e cria sua funcao no javascript

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function logar() {
+  const login = document.getElementById('input-login');
+  const senha = document.getElementById('input-password');
+  if (login.value === 'tryber@teste.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+
+const botao = document.getElementById('submit-login');
+botao.addEventListener('click', logar);
+
 function enviar(event) {
   const sendButton = document.getElementById('submit-btn');
   if (event.target.value === 'on') {
