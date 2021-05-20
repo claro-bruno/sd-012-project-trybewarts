b091d014c886356f33b6399a7466b5c2ba569bc5
David Azevedo <zevid.dev@outlook.com>
2021-05-20 07:28:51 -0300

    Realizado requisito 21

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const checkboxAgreement = document.querySelector('#agreement');
 const btnSubmit = document.querySelector('#submit-btn');
 const textarea = document.querySelector('#textarea');
 const counter = document.querySelector('#counter');
+const inputForm = document.querySelector('#evaluation-form');
+
+const inputName = document.querySelector('#input-name');
+const inputLastName = document.querySelector('#input-lastname');
+const inputEmail = document.querySelector('#input-email');
+const inputHouse = document.querySelector('#house');
+const inputFamily = document.querySelector('input[name="family"]:checked');
+const inputContentLike = document.querySelectorAll('input[name="matter"]:checked');
+const inputRate = document.querySelector('input[name="rate"]:checked');
+const inputTextare = document.querySelector('#textarea');
 
 function verificationInputsLogin() {
   const inputLogin = document.querySelector('#input-login');
@@ -35,4 +45,42 @@ textarea.addEventListener('input', () => {
   const caracteresRestantes = maxCaracteres - caracteresDigitados;
 
   counter.innerText = caracteresRestantes;
+});
+
+function createElementP(inputValue) {
+  const elementP = document.createElement('p');
+  elementP.innerHTML = inputValue;
+  inputForm.appendChild(elementP);
+}
+
+function matterValue(arrayValue) {
+  let inputValue = 'Matérias:';
+  for (let index = 0; index < arrayValue.length; index += 1) {
+    inputValue += `${arrayValue[index].value}`;
+    if (index !== arrayValue.length - 1) inputValue += ',';
+  }
+  createElementP(inputValue);
+}
+
+function changeValueInput() {
+  inputForm.innerHTML = '';
+
+  const fullName = `Nome: ${inputName.value} ${inputLastName.value}`;
+  createElementP(fullName);
+  const email = `Email: ${inputEmail.value}`;
+  createElementP(email);
+  const house = `Casa: ${inputHouse.value}`;
+  createElementP(house);
+  const family = `Família: ${inputFamily.value}`;
+  createElementP(family);
+  matterValue(inputContentLike);
+  const rate = `Nota: ${inputRate.value}`;
+  createElementP(rate);
+  const comment = `Observações: ${inputTextare.value}`;
+  createElementP(comment);
+}
+
+btnSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+  changeValueInput();
 });
