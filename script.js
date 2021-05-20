62d02b8dc48d52485c4b97649e089ece93b1235f
paulomichael <paulo.michael@gmail.com>
2021-05-20 04:31:33 -0300

    conteudo do formulario eh substituido pelas informacoes preenchidas

--- script.js
+++ script.js
@@ -39,26 +39,60 @@ function countChar() {
 
 textBox.addEventListener('keyup', countChar);
 
-/*
-class="subject"
-Nome
-Sobrenome
-Email
-Casa
-Família
-Matérias
-Avaliação
-Observações
- */
-/* const inputNome = document.getElementById('input-name')
-const inputSobrenome = document.getElementById('input-lastname')
+const searchParams = new URLSearchParams(window.location.search);
+const queryName = searchParams.get('name')
+const queryLastName = searchParams.get('lastname')
+const queryEmail = searchParams.get('email')
+const queryHouse = searchParams.get('casa')
+const queryFamily = searchParams.get('family')
+const queryConteudo = searchParams.getAll('conteudo')
+const queryRate = searchParams.get('rate')
+const queryComment = searchParams.get('comentario')
+
+const inputName = document.getElementById('input-name')
+const inputLastName = document.getElementById('input-lastname')
 const inputEmail = document.getElementById('input-email')
-const inputCasa = document.getElementById('')
-const inputFamília = document.getElementsByName('')
-const inputMatérias = document.getElementById('')
-const inputAvaliação = document.getElementById('')
-const inputObservações = document.getElementById('textarea')
-let valores = {
+const inputHouse = document.getElementById('house')
+const inputLabelFamily = document.getElementById('label-family')
+const inputLabelContent = document.getElementById('label-content')
+const inputLabelRate = document.getElementById('label-rate')
+const inputTextArea = document.getElementById('textarea')
+
+inputName.value = queryName;
+inputLastName.value = queryLastName;
+inputEmail.value = queryEmail;
+inputHouse.value = queryHouse;
+inputLabelFamily.value = queryFamily;
+inputLabelContent.value = queryConteudo;
+inputLabelRate.value = queryRate;
+inputTextArea.value = queryComment;
+
+// console.log(queryFamily)
+const radioButtonFamily = document.querySelectorAll('input[name="family"]')
+for(const radioButton of radioButtonFamily) {
+  if(queryFamily && radioButton.id === queryFamily.toLowerCase()) {
+    radioButton.checked = true;
+  }
+  // console.log(radioButton.id)
 }
-checkBox.addEventListener('click', activateSubmit);
-*/
+
+const radioButtonRate = document.querySelectorAll('input[name="rate"]')
+for(const radioButton of radioButtonRate) {
+  if(radioButton.id.split('-')[1] === queryRate) {
+    radioButton.checked = true;
+  }
+  // console.log(radioButton.id)
+  // console.log(radioButton.id.split('-')[1])
+}
+
+const checkBoxContent = document.querySelectorAll('input[name="conteudo"]')
+// console.log(checkBoxContent)
+
+for(let i = 0; i < queryConteudo.length; i += 1) {queryConteudo[i] = queryConteudo[i].toLowerCase()}
+console.log('queryConteudo',queryConteudo)
+for(const singleCheckBox of checkBoxContent) {
+  // console.log('singleCheckBox', singleCheckBox)
+  if(queryConteudo.includes(singleCheckBox.id.toLowerCase())) {
+    singleCheckBox.checked = true;
+  }
+}
