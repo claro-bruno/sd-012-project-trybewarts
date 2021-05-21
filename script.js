93452f29445c8be0effe044cec24ca19bf50d758
Ana Laura <analauraalves25@gmail.com>
2021-05-21 10:08:08 -0300

    Adiciona campo de avaliaçao e estilizaçao

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function verifyLogin(event) {
 }
 
 loginButton.addEventListener('click', verifyLogin);
+
+function generateRates() {
+  const contentRate = document.querySelector('.content-rate');
+  for (let index = 1; index <= 10; index += 1) {
+    let label = document.createElement('label');
+    let option = document.createElement('input');
+    option.classList.add('optionClass');
+    option.id = 'optionRate' + index;
+    label.for = option.id;
+    option.type = 'radio';
+    option.value = index;
+    option.name = 'rate';
+    label.innerHTML = index;
+    contentRate.appendChild(label);
+    label.appendChild(option);
+  }
+}
+
+generateRates();
