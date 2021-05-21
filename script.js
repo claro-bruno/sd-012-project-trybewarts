49d931b96d19209cd3e864fd2bb466ae8e4ead20
paulomichael <paulo.michael@gmail.com>
2021-05-20 23:39:46 -0300

    resolve lint

--- script.js
+++ script.js
@@ -40,23 +40,23 @@ function countChar() {
 textBox.addEventListener('keyup', countChar);
 
 const searchParams = new URLSearchParams(window.location.search);
-const queryName = searchParams.get('name')
-const queryLastName = searchParams.get('lastname')
-const queryEmail = searchParams.get('email')
-const queryHouse = searchParams.get('casa')
-const queryFamily = searchParams.get('family')
-const queryConteudo = searchParams.getAll('conteudo')
-const queryRate = searchParams.get('rate')
-const queryComment = searchParams.get('comentario')
+const queryName = searchParams.get('name');
+const queryLastName = searchParams.get('lastname');
+const queryEmail = searchParams.get('email');
+const queryHouse = searchParams.get('casa');
+const queryFamily = searchParams.get('family');
+const queryConteudo = searchParams.getAll('conteudo');
+const queryRate = searchParams.get('rate');
+const queryComment = searchParams.get('comentario');
 
-const inputName = document.getElementById('input-name')
-const inputLastName = document.getElementById('input-lastname')
-const inputEmail = document.getElementById('input-email')
-const inputHouse = document.getElementById('house')
-const inputLabelFamily = document.getElementById('label-family')
-const inputLabelContent = document.getElementById('label-content')
-const inputLabelRate = document.getElementById('label-rate')
-const inputTextArea = document.getElementById('textarea')
+const inputName = document.getElementById('input-name');
+const inputLastName = document.getElementById('input-lastname');
+const inputEmail = document.getElementById('input-email');
+const inputHouse = document.getElementById('house');
+const inputLabelFamily = document.getElementById('label-family');
+const inputLabelContent = document.getElementById('label-content');
+const inputLabelRate = document.getElementById('label-rate');
+const inputTextArea = document.getElementById('textarea');
 
 inputName.value = queryName;
 inputLastName.value = queryLastName;
@@ -68,31 +68,31 @@ inputLabelRate.value = queryRate;
 inputTextArea.value = queryComment;
 
 // console.log(queryFamily)
-const radioButtonFamily = document.querySelectorAll('input[name="family"]')
-for(const radioButton of radioButtonFamily) {
-  if(queryFamily && radioButton.id === queryFamily.toLowerCase()) {
+const radioButtonFamily = document.querySelectorAll('input[name="family"]');
+for (const radioButton of radioButtonFamily) {
+  if (queryFamily && radioButton.id === queryFamily.toLowerCase()) {
     radioButton.checked = true;
   }
   // console.log(radioButton.id)
 }
 
-const radioButtonRate = document.querySelectorAll('input[name="rate"]')
-for(const radioButton of radioButtonRate) {
-  if(radioButton.id.split('-')[1] === queryRate) {
+const radioButtonRate = document.querySelectorAll('input[name="rate"]');
+for (const radioButton of radioButtonRate) {
+  if (radioButton.id.split('-')[1] === queryRate) {
     radioButton.checked = true;
   }
   // console.log(radioButton.id)
   // console.log(radioButton.id.split('-')[1])
 }
 
-const checkBoxContent = document.querySelectorAll('input[name="conteudo"]')
+const checkBoxContent = document.querySelectorAll('input[name="conteudo"]');
 // console.log(checkBoxContent)
 
-for(let i = 0; i < queryConteudo.length; i += 1) {queryConteudo[i] = queryConteudo[i].toLowerCase()}
-console.log('queryConteudo',queryConteudo)
-for(const singleCheckBox of checkBoxContent) {
+for (let i = 0; i < queryConteudo.length; i += 1) { queryConteudo[i] = queryConteudo[i].toLowerCase(); }
+console.log('queryConteudo', queryConteudo);
+for (const singleCheckBox of checkBoxContent) {
   // console.log('singleCheckBox', singleCheckBox)
-  if(queryConteudo.includes(singleCheckBox.id.toLowerCase())) {
+  if (queryConteudo.includes(singleCheckBox.id.toLowerCase())) {
     singleCheckBox.checked = true;
   }
 }
