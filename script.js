054b9b86723c36247f72ac7f6cf8fdaf195c9c9d
Pedro Pinheiro <ph.santos.pinheiro@hotmail.com>
2021-05-21 20:27:40 -0300

    tentando pega informacoes atuais git pull

---
+++ script.js
@@ -0,0 +1,13 @@
+const email = document.querySelector('#mail');
+const senha = document.querySelector('#password');
+const submit = document.querySelector('#submit');
+
+submit.addEventListener('click', () =>{
+  const loginTrybe = 'tryber@teste.com'; 
+  const senhaTrybe = '123456';
+  if(email.value !== loginTrybe && senha.value !== senhaTrybe){
+      window.alert( "Login ou senha inválidos.")
+  } else {
+      window.alert( "Olá, Tryber!")
+  }    
+});
