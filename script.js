91a46eb975f0b22f8f63d7c6d412dad7fc87164c
Rodrigo <rodrigo.facury14@gmail.com>
2021-05-20 23:29:32 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const login = document.getElementById('login');
 const senha = document.getElementById('senha');
 const botao = document.getElementById('btn');
+const enviar = document.getElementById('submit-btn');
+const agree = document.getElementById('agreement');
 
 function alerta() {
   if (login.value !== 'tryber@teste.com' || senha.value !== '123456') {
@@ -11,3 +13,15 @@ function alerta() {
 }
 
 botao.addEventListener('click', alerta);
+
+enviar.disabled = true;
+
+function turnOnButton() {
+  if (enviar.disabled === true) {
+    enviar.disabled = false;
+  } else {
+    enviar.disabled = true;
+  }
+}
+
+agree.addEventListener('click', turnOnButton);
