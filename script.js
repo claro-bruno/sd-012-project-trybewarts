a41ee752345d8357039ea6d7b27d1c0066075b5e
Victor Veloso <victorveloso13@gmail.com>
2021-05-21 15:47:37 -0300

    Habilita/desabilita botao enviar

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const captureEmail = document.getElementById('login');
 const capturePassword = document.getElementById('senha');
 const captureBtnLogin = document.getElementById('logar');
-
+const captureBtnSubmit = document.getElementById('submit-btn');
+const captureCheckbox = document.getElementById('agreement');
+captureBtnSubmit.disabled = true
+//Para acessar Login e Senha
 function acessLogin(event) {
   console.log(captureEmail, capturePassword);
   if (captureEmail.value === 'tryber@teste.com' && capturePassword.value === '123456') {
@@ -13,3 +16,15 @@ function acessLogin(event) {
 }
 
 captureBtnLogin.addEventListener('click', acessLogin);
+
+//Para habilitar/desabilitar botão submit
+
+function controlBtn() {
+  if (captureCheckbox.checked) {
+    captureBtnSubmit.disabled = false;
+  } else {
+    (captureBtnSubmit.disabled = true);
+  }
+}
+
+captureCheckbox.addEventListener('click', controlBtn);
