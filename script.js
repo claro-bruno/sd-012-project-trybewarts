ea7c9b3aa425b310a3188e09dc7e6cf941c96785
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-05-22 16:31:18 -0300

    arquiv html: tirei o input family duplicado, arquivo script.js: subi as variaveis const, deci a validate function

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-function validate() {
   const inputLogin = document.querySelector('.input-login');
   const inputSenha = document.querySelector('.input-senha');
   const btnLogin = document.querySelector('.btn-login');
+  const agreement = document.querySelector('#agreement');
+  const enviar = document.querySelector('#submit-btn');
+  const submitButton = document.querySelector('#submit');
 
+const validate = () =>  { 
   btnLogin.addEventListener('click', () => {
     if (inputLogin.value !== 'tryber@teste.com' && inputSenha.value !== '123456') {
       alert('Login ou senha inválidos.');
     } else {
       alert('Olá, Tryber!');
     }
-  }
-};
-  const submitButton = document.querySelector('#submit');
+  });
+}
+
+
   submitButton.addEventListener('click', validate);
 
-  const agreement = document.querySelector('#agreement');
-  const enviar = document.querySelector('#submit-btn');
-  agreement.addEventListener('click', () => {
-    if (agreement.checked) {
-      enviar.disabled = false;
-    } else {
-      enviar.disabled = true;
-    }
-  });
-
+  // agreement.addEventListener('click', () => {
+  //   if (agreement.checked) {
+  //     enviar.disabled = false;
+  //   } else {
+  //     enviar.disabled = true;
+  //   }
+  // })
