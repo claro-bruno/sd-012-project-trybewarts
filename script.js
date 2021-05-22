e62b8086f0e5d78ca4a1a9f88bb31b887a85ad12
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-05-22 16:56:11 -0300

    arruamndo o lint do arquivo script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-  const inputLogin = document.querySelector('.input-login');
-  const inputSenha = document.querySelector('.input-senha');
-  const btnLogin = document.querySelector('.btn-login');
-  const agreement = document.querySelector('#agreement');
-  const enviar = document.querySelector('#submit-btn');
-  const submitButton = document.querySelector('#submit');
+const inputLogin = document.querySelector('.input-login');
+const inputSenha = document.querySelector('.input-senha');
+const btnLogin = document.querySelector('.btn-login');
+const agreement = document.querySelector('#agreement');
+const enviar = document.querySelector('#submit-btn');
+const submitButton = document.querySelector('#submit');
 
-const validate = () =>  { 
+const validate = () => {
   btnLogin.addEventListener('click', () => {
     if (inputLogin.value !== 'tryber@teste.com' && inputSenha.value !== '123456') {
       alert('Login ou senha inválidos.');
@@ -13,15 +13,14 @@ const validate = () =>  {
       alert('Olá, Tryber!');
     }
   });
-}
+};
 
+submitButton.addEventListener('click', validate);
 
-  submitButton.addEventListener('click', validate);
-
-  // agreement.addEventListener('click', () => {
-  //   if (agreement.checked) {
-  //     enviar.disabled = false;
-  //   } else {
-  //     enviar.disabled = true;
-  //   }
-  // })
+agreement.addEventListener('click', () => {
+  if (agreement.checked) {
+    enviar.disabled = false;
+  } else {
+    enviar.disabled = true;
+  }
+});
