ae11a63ee41e1066d6ecdf0213f7d8153eddc23a
walttoncoelho <walltton@outlook.com>
2021-05-22 13:56:52 -0300

    18 e 20

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ window.onload = function main() {
   const logar = document.querySelector('.acess');
   logar.addEventListener('click', login);
 };
+const agreed = document.getElementById('agreement');
+  agreed.addEventListener('click', checked);
+
+function checked() {
+  const agreed = document.getElementById('agreement');
+  const send = document.getElementById('submit-btn');
+  if (agreed.checked) {
+    send.removeAttribute('disabled');
+  } else {
+    send.setAttribute('disabled', 'disabled');
+  }
+}
+
+
+
+
+const counter = document.getElementById('counter');
+const textArea = document.getElementById('textarea');
+const maxCaracter = 500;
+counter.innerHTML = maxCaracter;
+textArea.addEventListener('input', () =>{
+  counter.innerHTML = maxCaracter - textArea.value.length;
+});
