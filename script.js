9c6f3beaec3c00c7275caaf27023983666c530fd
Pedro Pinheiro <ph.santos.pinheiro@hotmail.com>
2021-05-21 21:50:27 -0300

    requisito 18

--- script.js
+++ script.js
@@ -13,17 +13,8 @@ submit.addEventListener('click', () =>{
   }    
 });
 
-const login = document.querySelector('#login');
-const senha = document.querySelector('#password');
 
-const botao = document.querySelector('#buttonSubmit');
-botao.addEventListener('click', () => {
-  const loginTrybe = 'tryber@teste.com';
-  const senhaTrybe = '123456';
-  if (login.value !== loginTrybe && senha.value !== senhaTrybe) {
-    window.alert('Login ou senha inválidos.');
-  } else {
-    window.alert('Olá, Tryber!');
-  }
-});
 
+function myFunction() {
+  document.getElementById("submit-btn").disabled = false;
+}
