87309586abb35237c634d9de96883d6bde655841
Vinincius-Naoke <viniciusnaokert@gmail.com>
2021-05-22 16:29:51 -0300

    adiciona e estiliza text area, avaliador e adiciona funcao de contador

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const capturePassword = document.getElementById('senha');
 const captureBtnLogin = document.getElementById('logar');
 const captureBtnSubmit = document.getElementById('submit-btn');
 const captureCheckbox = document.getElementById('agreement');
-captureBtnSubmit.disabled = true
-//Para acessar Login e Senha
+const whatWasTyped = document.getElementById('textarea');
+const counter = document.getElementById('counter');
+captureBtnSubmit.disabled = true;
+//  Para acessar Login e Senha
 function acessLogin(event) {
   console.log(captureEmail, capturePassword);
   if (captureEmail.value === 'tryber@teste.com' && capturePassword.value === '123456') {
@@ -17,7 +19,7 @@ function acessLogin(event) {
 
 captureBtnLogin.addEventListener('click', acessLogin);
 
-//Para habilitar/desabilitar botão submit
+//  Para habilitar/desabilitar botão submit
 
 function controlBtn() {
   if (captureCheckbox.checked) {
@@ -28,3 +30,16 @@ function controlBtn() {
 }
 
 captureCheckbox.addEventListener('click', controlBtn);
+
+whatWasTyped.addEventListener('input', (e) => {
+  const {
+    target,
+  } = e;
+
+  const maxLength = target.getAttribute('maxlength');
+
+  const currentLength = target.value.length;
+
+  counter.innerHTML = maxLength - currentLength;
+
+});
