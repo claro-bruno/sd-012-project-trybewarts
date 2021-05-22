c0c4941a06d1e2eb827f22917f1e19feeafa59a8
Gui De Paula <guilhermeviniciustrabalho@gmail.com>
2021-05-22 17:12:46 -0300

    tirei o submit button e o evento de click dele, e indentei da linha 7 a 13

--- script.js
+++ script.js
@@ -3,19 +3,14 @@ const inputSenha = document.querySelector('.input-senha');
 const btnLogin = document.querySelector('.btn-login');
 const agreement = document.querySelector('#agreement');
 const enviar = document.querySelector('#submit-btn');
-const submitButton = document.querySelector('#submit');
 
-const validate = () => {
-  btnLogin.addEventListener('click', () => {
-    if (inputLogin.value !== 'tryber@teste.com' && inputSenha.value !== '123456') {
-      alert('Login ou senha inválidos.');
-    } else {
-      alert('Olá, Tryber!');
-    }
-  });
-};
-
-submitButton.addEventListener('click', validate);
+btnLogin.addEventListener('click', () => {
+  if (inputLogin.value !== 'tryber@teste.com' && inputSenha.value !== '123456') {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+});
 
 agreement.addEventListener('click', () => {
   if (agreement.checked) {
