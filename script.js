71d4cbecc0f50e14ef5b18e179b1a7bf6cd10951
walttoncoelho <walltton@outlook.com>
2021-05-22 16:21:02 -0300

    Questao 21

--- script.js
+++ script.js
@@ -8,12 +8,7 @@ function login() {
   }
 }
 
-window.onload = function main() {
-  const logar = document.querySelector('.acess');
-  logar.addEventListener('click', login);
-};
-
-function checked() {
+function check() {
   const agreed = document.getElementById('agreement');
   const send = document.getElementById('submit-btn');
   if (agreed.checked) {
@@ -22,13 +17,70 @@ function checked() {
     send.setAttribute('disabled', 'disabled');
   }
 }
-const agreed = document.getElementById('agreement');
-agreed.addEventListener('click', checked);
 
-const counter = document.getElementById('counter');
-const textArea = document.getElementById('textarea');
-const maxCaracter = 500;
-counter.innerHTML = maxCaracter;
-textArea.addEventListener('input', () => {
-  counter.innerHTML = maxCaracter - textArea.value.length;
-});
+function familia() {
+  const famil = document.getElementsByName('family');
+  const change = document.getElementById('familiaEscolhida');
+  for (let index = 0; index < famil.length; index += 1) {
+    if (famil[index].checked) {
+      change.innerHTML = `Família: ${famil[index].value}`;
+    }
+  }
+}
+
+function muda() {
+  const cont = document.getElementsByName('conteudo');
+  const change = document.getElementById('conteudo');
+  const array = [];
+  for (let index = 0; index < cont.length; index += 1) {
+    if (cont[index].checked) {
+      array.push(` ${cont[index].value}`);
+    }
+  }
+  change.innerHTML = `Matérias: ${array}`;
+}
+
+function avaliacao() {
+  const rate = document.getElementsByName('rate');
+  const change = document.getElementById('rate');
+  for (let index = 0; index < rate.length; index += 1) {
+    if (rate[index].checked) {
+      change.innerHTML = `Avaliação: ${rate[index].value}`;
+    }
+  }
+}
+
+function submited() {
+  const nome = document.getElementById('nomeDoTryber');
+  const emailCasa = document.getElementById('casaEmail');
+  const personName = document.getElementById('input-name').value;
+  const text = document.getElementById('textarea').value;
+  const textArea = document.getElementById('coment');
+  const lastName = document.getElementById('input-lastname').value;
+  const mail = document.getElementById('input-email').value;
+  const casa = document.getElementById('house').value;
+  nome.innerHTML = `Nome: ${personName} ${lastName}`;
+  emailCasa.innerHTML = `Email: ${mail} Casa: ${casa}`;
+  familia();
+  muda();
+  avaliacao();
+  textArea.innerHTML = `Observações: ${text}`;
+}
+window.onload = function main() {
+  const counter = document.getElementById('counter');
+  const textArea = document.getElementById('textarea');
+  const maxCaracter = 500;
+  counter.innerHTML = maxCaracter;
+  textArea.addEventListener('input', () => {
+    counter.innerHTML = maxCaracter - textArea.value.length;
+  });
+  const logar = document.querySelector('.acess');
+  logar.addEventListener('click', login);
+  const agreed = document.getElementById('agreement');
+  agreed.addEventListener('click', check);
+  const submit = document.getElementById('submit-btn');
+  submit.addEventListener('click', (Event) => {
+    Event.preventDefault();
+    submited();
+  });
+};
