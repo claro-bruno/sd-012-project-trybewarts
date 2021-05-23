81c87338aa830f4f1ecbbfe1051707861e20a6ac
Adriana Biberg <adribiberg@hotmail.com>
2021-05-23 19:03:17 -0300

    Adiciona alteraçoes, sem concluir exercicio 21

--- script.js
+++ script.js
@@ -23,43 +23,6 @@ function ability() {
   }
 }
 
-checkbox.addEventListener('click', ability);
-
-const form = document.getElementsByClassName('form-data');
-const btnForm = document.querySelector('#submit-btn');
-const nome = document.getElementsByClassName('nome-completo')[0].children;
-const email = document.getElementById('input-email');
-const main = document.getElementsByClassName('main');
-
-function captureElements() {
-  const nomeCompleto = document.createElement('div');
-  nomeCompleto.innerHTML = 'Nome: ';
-  nomeCompleto.classList.add('consolidado');
-  for (let index = 0; index < nome.length; index += 1) {
-    nomeCompleto.innerHTML += `${nome[index].value} `;
-  }
-
-  const inputEmail = document.createElement('div');
-  inputEmail.innerHTML = `E-mail: ${email.value}`;
-  inputEmail.classList.add('consolidado');
-
-  const divConsolida = document.createElement('div');
-  divConsolida.classList.add('consolidaContainer');
-  divConsolida.appendChild(nomeCompleto);
-  divConsolida.appendChild(inputEmail);
-  main[0].appendChild(divConsolida);
-}
-
-
-function formChange() {
-  for (let indexForm = 0; indexForm < form.length; indexForm += 1) {
-    form[indexForm].remove();
-  }
-}
-
-btnForm.addEventListener('click', captureElements);
-btnForm.addEventListener('click', formChange);
-
 /* Código do desafio 20 */
 
 let valorContador = 500;
@@ -72,3 +35,5 @@ function contaCaracter() {
 }
 
 comentario.addEventListener('keyup', contaCaracter);
+
+checkbox.addEventListener('click', ability);
