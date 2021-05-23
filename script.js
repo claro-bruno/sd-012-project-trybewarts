f0b7188cbad3bf6e60b32cd5f9ed20e513e7ddad
Gabriel Viana <g.viana87@hotmail.com>
2021-05-23 10:26:55 -0300

    stylint

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 const pegabotao = document.querySelector('#botao');
 function clicarLogar() {
-  let email1 = document.getElementById('email').value;
-  let password2 = document.getElementById('password').value;
+  const email1 = document.getElementById('email').value;
+  const password2 = document.getElementById('password').value;
   if (email1 === 'tryber@teste.com' && password2 === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
-
-  }
-}
+  };
+};
 
 pegabotao.addEventListener('click', clicarLogar);
 
 const checkzinho = document.getElementsByClassName('abriu')[0];
 
 function clickbait () {
-  let botaocheck = document.querySelector('.fechou');
+  const botaocheck = document.querySelector('.fechou');
   botaocheck.disabled = false;
-}
+};
 
 checkzinho.addEventListener('click', clickbait);
