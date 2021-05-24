c86d1b38c51d9fc23ab179945dd6ac6ba694ade5
Wellington <junior.wel02@exemplo.br>
2021-05-24 19:36:06 -0300

    21

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const botaoLogar = document.querySelector('#botaoLogar');
 const login = document.querySelector('#login');
 const senha = document.querySelector('#senha');
 const submitBtn = document.getElementById('submit-btn');
+const inputName = document.querySelector('#input-name');
+const inputLastN = document.querySelector('#input-lastname');
+const inputEmail = document.querySelector('#input-email');
+const house = document.querySelector('#house');
+const textArea = document.querySelector('#textarea');
 
 botaoLogar.addEventListener('click', () => {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -35,3 +40,49 @@ function CharacterLimit() {
   pCounter.innerHTML = tMaxLength - counter;
 }
 CharacterLimit();
+
+function GetFamilySelected() {
+  const family = document.getElementsByName('family');
+  for (let index = 0; index < family.length; index += 1) {
+    if (family[index].checked) {
+      return family[index].value;
+    }
+  }
+}
+
+function GetSubjectsSelected() {
+  const subjects = document.getElementsByClassName('subject');
+  let subjectsSelected = '';
+  for (let index = 0; index < subjects.length; index += 1) {
+    if (subjects[index].checked) {
+      subjectsSelected += `${subjects[index].value}, `;
+    }
+  }
+  return subjectsSelected;
+}
+
+function GetUserEvaluation() {
+  const evaluation = document.getElementsByName('rate');
+  for (let index = 0; index < evaluation.length; index += 1) {
+    if (evaluation[index].checked) {
+      return evaluation[index].value;
+    }
+  }
+}
+
+function FillInputs() {
+  const familySelected = GetFamilySelected();
+  const subjectsSelected = GetSubjectsSelected();
+  const userEvaluation = GetUserEvaluation();
+  const form = document.getElementsByTagName('form')[1];
+  form.innerHTML = '';
+  form.innerHTML += `Nome: ${inputName.value} ${inputLastN.value} <br>`;
+  form.innerHTML += `Email: ${inputEmail.value} <br>`;
+  form.innerHTML += `Casa: ${house.value} <br>`;
+  form.innerHTML += `Família: ${familySelected} <br>`;
+  form.innerHTML += `Matérias: ${subjectsSelected} <br>`;
+  form.innerHTML += `Avaliação: ${userEvaluation} <br>`;
+  form.innerHTML += `Observações: ${textArea.value}`;
+}
+
+submitBtn.addEventListener('click', FillInputs);
