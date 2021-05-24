a210e6a1f0d3d48916ddd231284d15ada303646a
Henrique Remedios <henriqueremedios@gmail.com>
2021-05-24 18:28:11 -0300

    Identaçao corrigida

--- script.js
+++ script.js
@@ -6,26 +6,26 @@ const textarea = document.getElementById('textarea');
 const counter = document.getElementById('counter');
 
 function starLogin() {
-    if (logId.value === 'tryber@teste.com' && passwId.value === '123456') {
-        alert('Olá, Tryber!');
-    } else {
-        alert('Login ou senha inválidos.');
+  if (logId.value === 'tryber@teste.com' && passwId.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
     }
 }
 btn.addEventListener('click', starLogin);
 
 const activeBtn = () => {
-    if (document.getElementById('agreement').checked) {
-        document.getElementById('submit-btn').removeAttribute('disabled');
-    } else {
-        document.getElementById('submit-btn').setAttribute('disabled', 'disabled');
+  if (document.getElementById('agreement').checked) {
+    document.getElementById('submit-btn').removeAttribute('disabled');
+  } else {
+    document.getElementById('submit-btn').setAttribute('disabled', 'disabled');
     }
 };
 formsBtn.addEventListener('click', activeBtn);
 
 const textLimitCounter = () => {
-    const maxLength = 500;
-    document.getElementById('textarea').setAttribute('maxlength', maxLength);
-    counter.innerHTML = 500 - textarea.value.length;
+  const maxLength = 500;
+  document.getElementById('textarea').setAttribute('maxlength', maxLength);
+  counter.innerHTML = 500 - textarea.value.length;
 }
 textarea.addEventListener('keyup', textLimitCounter);
