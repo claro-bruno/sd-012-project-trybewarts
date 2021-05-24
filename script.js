abd695a0e38c317a00575319d3c31537276668e7
Wellington <junior.wel02@exemplo.br>
2021-05-24 19:12:16 -0300

    18

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const botaoLogar = document.querySelector('#botaoLogar');
 const login = document.querySelector('#login');
 const senha = document.querySelector('#senha');
+const submitBtn = document.getElementById('submit-btn');
 
 botaoLogar.addEventListener('click', () => {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
+  }
+});
+
+function ActivateButton() {
+  submitBtn.disabled = false;
+}
+
+function DeactivateButton() {
+  submitBtn.disabled = true;
+}
+
+const checkAgreement = document.getElementById('agreement');
+checkAgreement.addEventListener('change', () => {
+  if (checkAgreement.checked) {
+    ActivateButton();
+  } else {
+    DeactivateButton();
   }
 });
