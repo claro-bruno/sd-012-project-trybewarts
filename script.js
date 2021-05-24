f229e6ac76cd3784953f1d32f739bbf9b022fa1c
ViniciusMossi <vini_agg@hotmail.com>
2021-05-24 15:48:59 -0400

    Versao final

--- script.js
+++ script.js
@@ -54,3 +54,55 @@ textArea.addEventListener('input', () => {
   const digitado = maxCaracter - textArea.value.length;
   p.innerText = digitado;
 });
+
+const capturaRadio = (name) => {
+  const capRadio = document.getElementsByName(name);
+  for (let index = 0; index < capRadio.length; index += 1) {
+    if (capRadio[index].checked) {
+      return capRadio[index].value;
+    }
+  }
+};
+
+const capturaMaterias = (classe) => {
+  const capMaterias = document.getElementsByClassName(classe);
+  const array = [];
+  for (let i = 0; i < capMaterias.length; i += 1) {
+    if (capMaterias[i].checked) {
+      array.push(capMaterias[i].value);
+    }
+  }
+  return array;
+};
+
+const capturaItens = () => {
+  const name = document.getElementById('input-name').value;
+  const lastName = document.getElementById('input-lastname').value;
+  const capmail = document.getElementById('input-email').value;
+  const capCasa = document.getElementById('house').value;
+  const capFam = capturaRadio('family');
+  const clasMat = capturaMaterias('subject');
+  const capRte = capturaRadio('rate');
+  const capText = document.getElementById('textarea').value;
+  const capTods = [name, lastName, capmail, capCasa, capFam, clasMat, capRte, capText];
+  return capTods;
+};
+
+const criaParagrafo = () => {
+  const paragrafo = document.createElement('p');
+  const armazena = capturaItens();
+  paragrafo.innerHTML = `Nome: ${armazena[0]} ${armazena[1]}<br>`;
+  paragrafo.innerHTML += `Email: ${armazena[2]} <br>`;
+  paragrafo.innerHTML += `Casa: ${armazena[3]}<br>`;
+  paragrafo.innerHTML += `Família: ${armazena[4]}<br>`;
+  paragrafo.innerHTML += `Matérias: ${armazena[5]}<br>`;
+  paragrafo.innerHTML += `Avaliação: ${armazena[6]}<br>`;
+  paragrafo.innerHTML += `Observações: ${armazena[7]}<br>`;
+
+  const capturaForm = document.getElementById('evaluation-form');
+  capturaForm.innerHTML = '';
+  capturaForm.innerHTML = paragrafo.innerHTML;
+};
+
+sendbtn.addEventListener('click', preventDef);
+sendbtn.addEventListener('click', criaParagrafo);
