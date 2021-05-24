1ed627114477c4656feb94f30ec37b5bbf690fcd
Gabriel Viana <g.viana87@hotmail.com>
2021-05-24 19:47:46 -0300

    linter

--- script.js
+++ script.js
@@ -15,35 +15,10 @@ const checkzinho = document.getElementsByClassName('abriu')[0];
 
 function clickbait() {
   const botaocheck = document.querySelector('.fechou');
-  botaocheck.disabled = false
-  if(botaocheck.disabled == false) {
+  botaocheck.disabled = false;
+  if (botaocheck.disable) {
     botaocheck.style.backgroundColor = 'slateblue';
   }
 }
 
 checkzinho.addEventListener('click', clickbait);
-
-/* const textArea = document.querySelector('#text-area')
-function calculoText() {
-const visor = document.querySelector('#counter')
-visor.innerText = '500';
-const length = document.querySelector('#text-area')
-.value.length;
-const valueNum = paseInt(length);
-const cont = 500 - valueNum;
-visor.innerText = cont ;
-
-}
-textArea.addEventListener('input', calculoText);
- */
-const textArea = document.querySelector('#text-area')
-function calculoText() {
-let contador = document.querySelector('#counter').innerText = '500';
-let valueTxt = textArea.innerText.value;
-const valueNum = parseInt(valueTxt)
-const cont = 500 - valueNum;
-contador.innerText = cont;
-
-}
-
-textArea.addEventListener('keypress', calculoText)
