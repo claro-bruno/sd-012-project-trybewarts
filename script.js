35726c4bf1c7db70587cfcee6924ce9b823e1e43
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-23 21:05:52 -0300

    final commit

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const loginUser = document.getElementById('login-name');
 const loginPwd = document.getElementById('login-pwd');
 const agreeCheckBox = document.getElementById('agreement');
 const submitBtn = document.getElementById('submit-btn');
+const textArea = document.getElementById('textarea');
+const counter = document.getElementById('counter');
 submitBtn.disabled = true;
+submitBtn.style.display = 'none';
 
 function validateLogin(e) {
   e.preventDefault();
@@ -17,11 +20,18 @@ function validateLogin(e) {
 }
 loginSubmitBtn.addEventListener('click', validateLogin);
 
+function uptdateCount() {
+  counter.innerHTML = 500 - textArea.value.length;
+}
+
 function enableSubmitBtn() {
   if (agreeCheckBox.checked === true) {
     submitBtn.disabled = false;
+    submitBtn.style.display = 'block';
   } else {
     submitBtn.disabled = true;
+    submitBtn.style.display = 'none';
   }
 }
 agreeCheckBox.addEventListener('click', enableSubmitBtn);
+textArea.addEventListener('keyup', uptdateCount);
